d697dc0fb24f843166ab95aa2bfbeaa50645dd83
jonpan <jonathan.pan@gmail.com>
2022-06-29 19:01:24 -0700

    submitting lab01

---
+++ code/jonpan/lab01.py
@@ -0,0 +1,11 @@
+nums = [5, 0, 8, 3, 4, 1, 6]
+
+sum = 0 
+
+for num in nums:
+    sum = sum + num 
+
+average = sum/(len(nums))
+
+print(average)
+
---
+++ code/jonpan/lab02.py
@@ -0,0 +1,28 @@
+## VERSION 1 BELOW
+#distance = input("what is the distance in feet: ")  
+#meters = {
+#    'ft': 0.3048
+#}
+#a = int(distance)
+#result = 1
+#for i in meters:
+#    result = a*meters[i]
+#print(f"\n{distance} ft is {result} m")
+
+## VERSION 2 AND 3 COMBINED BELOW
+
+distance = input("\nWhat is the distance? ") 
+units = input("\nWhat are the units? Your choices are feet, miles, meters, km, yard, or inch. ")
+conversion = {
+    'feet': 0.3048,
+    'miles': 1609.34,
+    'meters': 1,
+    'km': 1000,
+    'yard': 0.9144,
+    'inch': 0.0254,
+}
+
+selectedunit = conversion[units]
+result = selectedunit * int(distance)
+
+print(result)
