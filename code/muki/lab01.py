62fd88af0aa989daece188039768f004504bb46e
Will Shelnutt <willshelnutt@gmail.com>
2022-06-29 19:02:07 -0700

    have been working back and forth test commit, and cya

--- code/muki/lab01.py
+++ code/muki/lab01.py
@@ -2,6 +2,20 @@
 
 # Version 1 
     # using given list for nums = [5, ,0, 8, 3, 4, 1, 6]
+    # thoughts are to run some loops and print sum functions with some math going on
+    # i will try to define a function that will do this stuff and use that for version 2
 
-def listmean(list):
-    list
+numbahs = [5, 0, 8, 3, 4, 1, 6]
+print(numbahs)
+print('\n') # trying to make this a little easier to identify
+
+for numbah in numbahs:
+    print(numbah)
+print('\n') # trying to make this a little easier to identify
+
+for i in range(len(numbahs)):
+    print(numbahs[i])
+print('\n') # trying to make this a little easier to identify
+print(sum(numbahs)/len(numbahs))
+
+
