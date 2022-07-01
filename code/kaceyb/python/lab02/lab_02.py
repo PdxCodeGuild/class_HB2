2e59da99da8c1179921f00b8917e0c37289cc880
Kacey Broetje <kaceylauinger@gmail.com>
2022-06-30 19:20:54 -0700

    finished converter lab02

---
+++ code/kaceyb/python/lab02/lab_02.py
@@ -0,0 +1,81 @@
+
+
+
+#         VERSION 1           #
+
+
+conversion_list = {
+    'foot': 0.3048,
+    'mile': 1609.34,
+    'meter': 1,
+    'kilometers': 1000
+    
+}
+
+# 
+
+# while True:
+    
+#     principle_unit = input("\nWhat is the distance in feet?: ")
+#     total = (conversion_list['foot']) * int(principle_unit)
+    
+#     print(f"{principle_unit} feet is in {total} m")
+    
+#     break
+
+
+#         VERSION 2            #
+
+# conversion_list = {
+#     'foot': 0.3048,
+#     'mile': 1609.34,
+#     'meter': 1,
+#     'kilometers': 1000
+    
+# }
+
+# user = True
+# while user:
+    
+#     users_unit_input = input("\nWhat is the unit to convert to meters?: ")
+    
+#     users_unit_input == conversion_list[users_unit_input]
+    
+#     print(users_unit_input)
+#     # print(users_distance_input)
+#     users_distance_input = int(input("\nWhat is the distance to convert to meters?: "))
+    
+#     distance = users_distance_input * conversion_list[users_unit_input]
+#     # print(distance)
+#     print(f"{users_distance_input} {users_unit_input} is {((users_distance_input) * conversion_list[users_unit_input])} meters")
+    
+#     user = False
+    
+#            VERSION 3               #
+
+# conversion_list = {
+#     'foot': 0.3048,
+#     'mile': 1609.34,
+#     'meter': 1,
+#     'kilometers': 1000,
+#     'yard': 0.9144,
+#     'inch': 0.0254
+    
+# }
+
+# user = True
+# while user:
+    
+#     users_unit_input = input("\nWhat is the unit to convert to meters?: ")
+    
+#     users_unit_input == conversion_list[users_unit_input]
+    
+#     print(users_unit_input)
+#     # print(users_distance_input)
+#     users_distance_input = int(input("\nWhat is the distance to convert to meters?: "))
+    
+#     distance = users_distance_input * conversion_list[users_unit_input]
+#     # print(distance)
+#     print(f"{users_distance_input} {users_unit_input} is {((users_distance_input) * conversion_list[users_unit_input])} meters")
+    
+#     user = False
