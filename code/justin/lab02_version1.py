eb582f0ec431e3d0f3c49d3a74d3f037ead73c26
justin-young5 <justin.young5@yahoo.com>
2022-07-01 09:25:33 -0500

    deleted a file and updated  lab2/V1

--- code/justin/lab02_version1.py
+++ code/justin/lab02_version1.py
@@ -7,11 +7,14 @@ Version1
 measurements = {'ft' : 0.3048, 'mi': 1609.34, 'm': 1, 'km': 1000}
 user_entry = input('Enter how many feet you wish to convert: ')
 
-try:
-    float(user_entry)
-except ValueError:
-    user_entry = input(f'Value entered is not an acceptable number try again: ')
-user_entry = float(user_entry)
+while True:
+    try:
+        float(user_entry)
+        user_entry = float(user_entry)
+        answer = user_entry * measurements['ft']
+        break
+    except ValueError:
+        user_entry = input(f'Value entered is not an acceptable number try again: ')
+        False
 
-answer = user_entry * measurements['ft']
 print(f'The number of meters for {user_entry} ft is {answer}')
