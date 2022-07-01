83a8db76e99773403f41960891b11f44413d90f2
Zachary Christman <zacharywchristman2@outlook.com>
2022-06-30 21:52:35 -0400

    zach-lab02-unit-converter

--- code/zach/lab02-unit-converter.py
+++ code/zach/lab02-unit-converter.py
@@ -1,26 +1,27 @@
-def version1():
-    
-    return answer
-
-def version2():
-    
-    return answer
-
-def version3():
-    
-    return answer
-
-def unit_conversion():
-    # Establish dictionary with values to convert meters to other measurements
-    conv = {
-        'ft': 0.3048,
-        'mi': 1609.34,
-        'm': 1,
-        'km': 1000,
-        'yd': 0.9144,
-        'in': 0.0254
+conv_dict = {
+    'ft': 0.3048,
+    'mi': 1609.34,
+    'm': 1,
+    'km': 1000,
+    'yd': 0.9144,
+    'in': 0.0254
     }
+
+def version1(conv = conv_dict):
+    feet = float(input("what is the distance in feet? "))
+    answer = f"{feet} ft is {feet * conv['ft']} m"
+
+    return answer
+
+def version2(conv = conv_dict):
+    input_distance = float(input('What is the distance? '))
+    input_measurement = str(input('What is the unit of measurement (ft, mi, m, km)? '))
     
+    answer = f'{input_distance} {input_measurement} is {input_distance * conv[input_measurement]} m'
+    
+    return answer
+
+def version3(conv = conv_dict):
     input_distance = float(input('What is the distance? '))
     input_measurement = str(input('What is the unit of measurement (ft, mi, m, km, yd, in)? '))
     
@@ -29,5 +30,8 @@ def unit_conversion():
     return answer
 
 def main():
-    
-    return 0
+    print(version1())
+    print(version2())
+    print(version3())
+
+main()
