b180df2a3660226883ff5b5dc95e15b114360557
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-07-01 00:10:11 -0400

    Unit Converter Lab Versions 1-3

--- code/kelin/Unit-converter.py
+++ code/kelin/Unit-converter.py
@@ -3,6 +3,9 @@
 # Version 1
 # Ask the user for the number of feet, and print out the equivalent distance in meters. Hint: 1 ft is 0.3048 m.
 
+from operator import truediv
+
+
 feet_to_meters = {'ft': 0.3048}
 
 number_feet = input("\nPlease enter the number of feet: ")
@@ -11,7 +14,7 @@ number_feet = int(number_feet)
 
 meters = (feet_to_meters['ft'] * number_feet)
 
-print(meters)
+print((number_feet), "feet is", (meters), "meters")
 
 # Version 2
 
@@ -21,26 +24,40 @@ print(meters)
 # 1 mi is 1609.34 m
 # 1 m is 1 m
 # 1 km is 1000 m
-# Below is some sample input/output:
-
-# > what is the distance? 100
-# > what are the units? mi
-# > 100 mi is 160934 m
 
 meter = {
     "ft": 0.3048,
     "mi": 1609.34,
     "m": 1,
-    "km": 1000
+    "km": 1000,
+    "yd": 0.9144,
+    "in": 0.0254
 }
 
-number = input("Enter the distance: ")
-unit = input("Enter the unit - ft - mi - m - km: ")
+while True:
+    number = input("Enter the distance: ")
+    unit = input("Enter the unit - ft - mi - m - km - yd - in: ")
+    if unit == 'ft':
+        print((number), "feet is", (int(number) * (meter["ft"])), "meters")
+    elif unit == 'mi':
+        print((number), "miles is", (int(number) * (meter["mi"])), "meters")
+    elif unit == 'm':
+        print((number), "meters is", (int(number) * (meter["m"])), "meters")
+    elif unit == 'km':
+        print((number), "kilometers is", (int(number) * (meter["km"])), "meters")
+    elif unit == 'yd':
+        print((number), "yards", (int(number) * (meter["yd"])), "meters")
+    elif unit == 'in':
+        print((number), "inches is", (int(number) * (meter["in"])), "meters")
+    else:
+        print("Invalid unit")
 
+# Verson 3
 
+# Added support for yards, and inches.
 
-
-
+# 1 yard is 0.9144 m
+# 1 inch is 0.0254 m
 
 
 
