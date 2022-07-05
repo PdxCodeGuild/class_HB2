1c9a2b9cce44c5fb55ec4a77ff7be82bc5cdf0c7
sushi <dandouglass14@gmail.com>
2022-07-04 23:59:20 -0400

    UnitConverter.py

--- 01_UnitConverter/UnitConverter.py
+++ 01_UnitConverter/UnitConverter.py
@@ -39,31 +39,24 @@
 # from turtle import distance
 
 
-enterUnitType = input("Choose a unit measurement (ft, mi, m, or km): ")
-enterDistance = input("Choose a distance: ")
-ftToMeters = float(enterDistance) * 0.3048
-miToMeters = float(enterDistance) * 1609.34
-mToMeters = float(enterDistance) 
-kmToMeters = float(enterDistance) * 1000.00
-# distanceTypes = {
-#     "ft": 0.3048,
-#     "mi": 1609.34,
-#     "m":1,
-#     "km": 1000
-# }    
-
-if enterUnitType == "ft":
-    print(f"{enterDistance} ft is {ftToMeters} meters.")
-elif enterUnitType == "mi":
-    print(f"{enterDistance} miles is {miToMeters} meters.")
-elif enterUnitType == "m":
-    print(f"{enterDistance} meters is {mToMeters} meters.")
-elif enterUnitType == "km":
-    print(f"{enterDistance} kilometers is {kmToMeters} meters.")
-else:
-    print("Not a valid option")
+# enterUnitType = input("Choose a unit measurement (ft, mi, m, or km): ")
+# enterDistance = input("Choose a distance: ")
+# ftToMeters = float(enterDistance) * 0.3048
+# miToMeters = float(enterDistance) * 1609.34
+# mToMeters = float(enterDistance) 
+# kmToMeters = float(enterDistance) * 1000.00
 
 
+# if enterUnitType == "ft":
+#     print(f"{enterDistance} ft is {ftToMeters} meters.")
+# elif enterUnitType == "mi":
+#     print(f"{enterDistance} miles is {miToMeters} meters.")
+# elif enterUnitType == "m":
+#     print(f"{enterDistance} meters is {mToMeters} meters.")
+# elif enterUnitType == "km":
+#     print(f"{enterDistance} kilometers is {kmToMeters} meters.")
+# else:
+#     print("Not a valid option")
 
 
 
@@ -77,6 +70,16 @@ else:
 
 # 1 yard is 0.9144 m
 # 1 inch is 0.0254 m
+
+enterUnitType = input("Choose a unit measurement (yd or in): ")
+enterDistance = input("Choose a distance: ")
+ftToMeters = float(enterDistance) * 0.9144
+miToMeters = float(enterDistance) * 0.0254
+
+if enterUnitType == "yd":
+    print(f"{enterDistance} yards is {ftToMeters} meters.")
+elif enterUnitType == "in":
+    print(f"{enterDistance} inches is {miToMeters} meters.")
 
 
 # ====================
