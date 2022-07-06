14b513ee47558da4e9d7d655245e7b900d0bf688
Kacey Broetje <kaceylauinger@gmail.com>
2022-07-05 18:29:14 -0700

    fixed

--- code/kaceyb/python/lab02/lab_02.py
+++ code/kaceyb/python/lab02/lab_02.py
@@ -4,8 +4,6 @@
 #         VERSION 1           #
 
 
-from inspect import formatannotation
-
 
 conversion_list = {
     'foot': 0.3048,
@@ -94,41 +92,41 @@ conversion_list = {
     
 #     user = False
     
-    #        VERSION 4         #
+        #    VERSION 4         #
     
     
-# conversion_list = {
-#     'foot': 0.3048,
-#     'feet': 0.3048,
-#     'mile': 1609.34,
-#     'miles': 1609.34,
-#     'meter': 1,
-#     'meters': 1,
-#     'kilometer': 1000,
-#     'kilometers': 1000,
-#     'yard': 0.9144,
-#     'yards': 0.9144,
-#     'inch': 0.0254,
-#     'inches': 0.0254
-#     }
+conversion_list = {
+    'foot': 0.3048,
+    'feet': 0.3048,
+    'mile': 1609.34,
+    'miles': 1609.34,
+    'meter': 1,
+    'meters': 1,
+    'kilometer': 1000,
+    'kilometers': 1000,
+    'yard': 0.9144,
+    'yards': 0.9144,
+    'inch': 0.0254,
+    'inches': 0.0254
+    }
 
-# user = True
-# while user: 
-#     users_distance_input = int(input("\nWhat is the distance to convert?: "))
+user = True
+while user: 
+    users_distance_input = int(input("\nWhat is the distance to convert?: "))
     
-#     users_unit_input = input("\nWhat is the unit to convert from?: ")
+    users_unit_input = input("\nWhat is the unit to convert from?: ")
     
-#     users_unit_input == conversion_list[users_unit_input]
+    users_unit_input == conversion_list[users_unit_input]
     
-#     users_unit_output = input("\nWhat is the unit to convert to?: ")
+    users_unit_output = input("\nWhat is the unit to convert to?: ")
     
-#     users_unit_output == conversion_list[users_unit_output]
+    users_unit_output == conversion_list[users_unit_output]
     
-#     result = users_distance_input * conversion_list[users_unit_input]
+    result = users_distance_input * conversion_list[users_unit_input]
     
-#     result = result / conversion_list[users_unit_output]
+    result = result / conversion_list[users_unit_output]
     
-#     print(f"{users_distance_input} {users_unit_input} is {result}")
+    print(f"{users_distance_input} {users_unit_input} is {result}")
         
-#     user = False
+    user = False
     
