4bb7ebe9755c34ae35c2ce445b2477efb0f6a841
Will Shelnutt <willshelnutt@gmail.com>
2022-07-05 18:35:42 -0700

    lab01.py

--- code/muki/lab01.py
+++ code/muki/lab01.py
@@ -5,17 +5,42 @@
     # thoughts are to run some loops and print sum functions with some math going on
     # i will try to define a function that will do this stuff and use that for version 2
 
+from importlib.abc import MetaPathFinder
+
+'''
 numbahs = [5, 0, 8, 3, 4, 1, 6]
 print(numbahs)
-print('\n') # trying to make this a little easier to identify
+print('\n') # trying to make this a little easier to identify when printed
 
 for numbah in numbahs:
     print(numbah)
-print('\n') # trying to make this a little easier to identify
+print('\n') # trying to make this a little easier to identify  when printed
 
 for i in range(len(numbahs)):
     print(numbahs[i])
-print('\n') # trying to make this a little easier to identify
+print('\n') # trying to make this a little easier to identify when printed
 print(sum(numbahs)/len(numbahs))
+'''
+'''
+def mean_from_input(total, pieces,): 
+    total = sum(total)
+    pieces = len(total)
+    meanie = total / pieces 
+    return meanie
+'''
+'''
+# Version 2
+days = input('How many are there?\t')
+days = int(days)
+months = input('How many groups should there be?\t')
+months = int(months)
+print(f'The average number of days in a month is: {mean_from_input(days, months)}')
+'''
+'''numbaahs = []
+while True:
+    n_entry = input("Please enter a number:\t")
+    numbaahs.append(n_entry)
+print(numbaahs)
+'''
 
-
+    
