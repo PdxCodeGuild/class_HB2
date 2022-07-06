30b28263e2f0b15f1fa4ad8742b7f184c4dca0e4
andy2124 <andygarcia22212@gmail.com>
2022-07-06 16:38:10 -0700

    still working on

--- code/Andy/python/lab01-average_number.py
+++ code/Andy/python/lab01-average_number.py
@@ -1,9 +1,24 @@
+# # Version 1
 nums = [5, 0, 8, 3, 4, 1, 6]
 
-total = 0
+# total = 0
 
-for num in nums:
-        total = total + num
+# for num in nums:
+#         total = total + num
 
-average = total/(len(nums))
-print(average)
+# average = total/(len(nums))
+# print(average)
+
+
+# for i in range(len(nums)):
+#      print(nums[i])
+
+# Version 2
+enter = input('Please enter or a number or enter done: ')
+
+nums = []
+while True:
+     n_entry = input("Please enter a number:\t")
+     nums.append(n_entry)
+     print(nums)
+
