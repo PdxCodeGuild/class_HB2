575f58e1d3e57b6525510ee264dd94f9023aadeb
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-07-07 00:21:48 -0400

    Working on lab 3

--- code/kelin/Labs/Number-to-phrases.py
+++ code/kelin/Labs/Number-to-phrases.py
@@ -17,25 +17,42 @@
 # Version 4 (optional)
 # Convert a time given in hours and minutes to a phrase.
 
-number_phrases = {0: 'zero',
-1: 'one',
-2: 'two',
-3: 'three',
-4: 'four',
-5: 'five',
-6: 'six',
-7: 'seven',
-8: 'eight',
-9: 'nine',
-10: 'ten',
-20: 'twenty',
-30: 'thirty',
-40: 'forty',
-50: 'fifty',
-60: 'sixy',
-70: 'seventy',
-80: 'eighty',
-90: 'ninety',
-}
+below_20 = {0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',}
+tens = {
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixy',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',}
 
-print(number_phrases)
+def number_phrase(num):
+    if num == 0:
+        return 'zero'
+    
+    while num > 0:
+
+num = input("Enter a number between 0 and 99: ")
+
+print(number_phrase)
