677f423eabf557c956ef872548d08599ce9252f0
andy2124 <andygarcia22212@gmail.com>
2022-07-06 18:09:04 -0700

    still working on

--- code/Andy/python/functions.py
+++ code/Andy/python/functions.py
@@ -92,6 +92,9 @@
 
 
 # print(convert_number(67))
+from hashlib import new
+
+
 low_numbers = { 
     0: "zero",
     1: "one",
@@ -106,7 +109,7 @@ low_numbers = {
     10: 'ten',
     11: 'eleven',
     12: 'tweleve',
-    13: 'thirty',
+    13: 'thirteen',
     14: 'fourteen',
     15: 'fifteen',
     16: 'sixteen',
@@ -145,24 +148,36 @@ def convert_number(number):
     # return number]
     if number < 20:
         return low_numbers[number]
-    
-    # elif number <  100:
+   
+    elif number <  100:
         
-    #     tens_digit = number//10
-    #     ones_digit = number%10
-    # return f"{ten[tens_digit]} - {low_numbers[ones_digit]}"   
+        tens_digit = number // 10
+        # print('10s digit: ', tens_digit)
+        ones_digit = number % 10 #takes whatever number and divides that by 10 and then gives the remainder
+        # print('1s digit: ', ones_digit)
+    # return f"{ten[tens_digit]} - {low_numbers[ones_digit]}" 
+        tens_word = ten[tens_digit]
+        ones_word = low_numbers[ones_digit]
+        return f'{tens_word + " " + ones_word}'
 
 
     elif number < 999:
-    
-        hundreds_digit = number//100 #floor divide regular division with no remainder
-        tens_digit = number//10
-        ones_digit = number%10
-    
-    
-    return f"{hundreds[hundreds_digit]} - {ten[tens_digit]} - {low_numbers[ones_digit]}"
+        
+        hundreds_digit = number // 100 #floor divide regular division with no remainder
+        tens_digit = number // 10 
+        new_tens = tens_digit % 10
+        ones_digit = number % 10
+        hundreds_word = hundreds[hundreds_digit]
+        tens_word = ten[new_tens]
+        ones_word = low_numbers[ones_digit]
+        return f'{hundreds_word + " " + tens_word + " " + ones_word}'    
+        # return hundreds_digit, tens_digit
+    # return f"{hundreds[hundreds_digit]} - {ten[tens_digit]} - {low_numbers[ones_digit]}"
 
+print(convert_number(532))
 
-print(convert_number(100))
+print(convert_number(111))
+print(convert_number(813))
+print(convert_number(140))
 
 
