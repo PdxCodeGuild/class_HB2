0cd74c1fd712b6c921764497adf21297cec6189e
andy2124 <andygarcia22212@gmail.com>
2022-07-07 16:00:21 -0700

    Lab 01 completed

--- code/Andy/python/lab01-average_number.py
+++ code/Andy/python/lab01-average_number.py
@@ -14,34 +14,21 @@
 #      print(nums[i])
 
 # Version 2
-nums = []
 
 
 
-Done = True
-while Done == True: 
-        user = input('Please enter or a number or enter "done" : ')
+nums = [] #place holder
+sum = 0 # place to help when adding the num together and help get the average 
 
-        if user != Done:
-                nums.append(int(user))
-        
-        else:
-                Done = False
+while True: #while statement to get all the numbers being put by the user
+        user= input(' Please enter a number or done when finished: ')
+        if user == 'done':
+                break
+        nums.append(int(user)) #strings dont add together for numbers, .append adding all the numbers in a list and the int making them into an integer.
+        # print(nums)
 
-        
-        
-        
-        
-        # if user != Done:
-        #         nums.append(int(user))
-        
-        # else:
-        #         Done = False 
+for num in nums: #for statement helping me get the nums value of all the inputs and calling out single ones
+        sum += num #where it calls the sigle ones and adds them together
+        average = sum/ len(nums) #the len is getting the length of numbers so getting how many there is and the sum is getting the value of all the ones being put together and dividing them to get the average
 
-sum = 0 
-for num in nums:
-        sum += num
-
-print(f' you entered {nums}')
-print(f'the average is {sum/len(nums)}')
-                
+print (f" Your total average is {round(average)} ")  #round removes the decimal and has to be within the curly bracket to have the function work
