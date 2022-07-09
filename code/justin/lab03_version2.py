988cdbdcb86ae2c4bf7f1dbd11eb4c095d55efb4
justin-young5 <justin.young5@yahoo.com>
2022-07-08 23:42:34 -0500

    fixed lab3V2

--- code/justin/lab03_version2.py
+++ code/justin/lab03_version2.py
@@ -5,34 +5,16 @@ Version2
 '''
 
 def n2p(number):
-    ones = {1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',0:'zero'}
+    ones = {1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',0:''}
     teens = {1:'eleven',2:'twelve',3:'thirteen',4:'fourteen',5:'fifteen',6:'sixteen',7:'seventeen',8:'eighteen',9:'nineteen'}
-    tens = {1:'ten',2:'twenty',3:'thirty',4:'fourty',5:'fifty',6:'sixty',7:'seventy',8:'eighty',9:'ninety'}
-    hundreds = {1:'one hundred',2:'two hundred'}
+    tens = {1:'ten',2:'twenty',3:'thirty',4:'fourty',5:'fifty',6:'sixty',7:'seventy',8:'eighty',9:'ninety',0:''}
     if number >= 100:
         a = number%10
-        b = number//10
+        b = number%100//10
         c = number//100
-        if b ==10:
-            if a ==0:
-                return hundreds[c]
-            elif a >=1 <=9:
-                return f'{hundreds[c]} {ones[a]}'
-        elif b ==11:
-            if a == 0:
-                return f'{hundreds[c]} {tens[1]}'
-            elif a >= 1 <= 9:
-                return f'{hundreds[c]} {teens[a]}'
-        elif b >= 12 <=19:
-            number = str(number)
-            second_digit = int(number[1])
-            first_digit = int(number[2])
-            if first_digit ==0:
-                return f'{hundreds[1]} {tens[second_digit]}'    
-            return f'{hundreds[1]} {tens[second_digit]} {ones[first_digit]}'
     elif number >= 20 <=99:
         a = number%10    
-        b = number//10    
+        b = number//10  
         if a == 0:
             return tens[b]
         else:
@@ -50,10 +32,10 @@ def n2p(number):
          
 while True:
     try:
-        user_num = input('Enter a number between 0-199: ')
+        user_num = input('Enter a number between 0-999: ')
         user_num = int(user_num)
-        if user_num >= 199:
-            print(f'number too high, 0-199 only pls')
+        if user_num >= 1000:
+            print(f'number too high, 0-999 only pls')
             continue
         break
     except:
