5e0d417956e7b0bd69934db69838fc48ac6f16fd
justin-young5 <justin.young5@yahoo.com>
2022-07-09 19:39:41 -0500

    finished ver4

--- code/justin/lab03_version4.py
+++ code/justin/lab03_version4.py
@@ -1,12 +1,14 @@
 
 
 def time_conv(x):
-    time={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve',13:'thirteen',14:'fourteen',15:'fifteen',16:'sixteen',
+    time={0:'',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve',13:'thirteen',14:'fourteen',15:'fifteen',16:'sixteen',
     17:'seventeen',18:'eighteen',19:'nineteen',20:'twenty',30:'thirty',40:'fourty',50:'fiftey'}
     a=int(x)//100
     b=int(x)%100
     hour=time[a]
-    if b <=20:
+    if b <10:
+        min=f"o'{time[b]}"
+    elif b <=20:
         min=time[b]
     elif b >20:
         ten_min=b//10*10
@@ -17,6 +19,6 @@ def time_conv(x):
 
 while True:
     user_time=input('Enter a time in hours and minutes, ex 1245 or done to stop: ')
-    print(time_conv(user_time))
     if user_time == 'done':
         break
+    print(time_conv(user_time))
