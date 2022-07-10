b69cf7d97655a895c5424329ba4d4abe88bce4a1
sushi <dandouglass14@gmail.com>
2022-07-10 18:12:51 -0400

    Lab3FinalVersion

--- code/daniel/03_NumberToPhrase/NumberToPhrase.py
+++ code/daniel/03_NumberToPhrase/NumberToPhrase.py
@@ -18,79 +18,13 @@
 # Convert a time given in hours and minutes to a phrase.
 #===================================================================================
 
-from ast import NotIn
 import numbers
-from xml.dom import WrongDocumentErr
 
-from cupshelpers import activateNewPrinter
 
 
 #====================================
 #Version1
 #====================================
-
-onesNumberList = {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine", 
-}
-tensNumberList = {
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "ninteen",
-    20: "twenty",
-    30: "thirty",
-    40: "fory",
-    50: "fifty",
-    60: "sixty",
-    70: "seventy",
-    80: "eighty",
-    90: "ninty",
-}
-
-
-pickANumber = input("Pick a number between 0 and 99: ")
-intNum = int(pickANumber)
-tens_digit = intNum//10
-ones_digit = intNum%10
-# print(tens_digit)
-# print(ones_digit)
-
-
-while intNum > 99 or intNum < 0:
-        print(f"Number is out of range")
-        exit()
-
-    # print(onesNumberList.get(int(6)))
-while True:
-    if intNum in onesNumberList:
-        print(str(onesNumberList[intNum]))
-        break
-    elif intNum in tensNumberList:
-        print(str(tensNumberList[intNum]))
-        break
-    elif intNum not in onesNumberList and intNum not in tensNumberList:
-        # wordNum = tensNumberList[tens_digit*10] + onesNumberList[ones_digit*10]
-        print(str(tensNumberList[tens_digit*10] + "-" + onesNumberList[ones_digit]))
-        break
- 
-#=======================================
-#Version2
-#=======================================
 
 # onesNumberList = {
 #     0: "zero",
@@ -125,38 +59,16 @@ while True:
 #     90: "ninty",
 # }
 
-# HundredNumberList = {
-#     100: "one hundred",
-#     200: "two hundred",
-#     300: "three hundred",
-#     400: "four hundred",
-#     500: "five hundred",
-#     600: "six hundred",
-#     700: "seven hundred",
-#     800: "eight hundred",
-#     900: "nine hundred", 
-# }
 
 # pickANumber = input("Pick a number between 0 and 99: ")
 # intNum = int(pickANumber)
-# wordNum = ""
-# hundred_digit = intNum//100
-
-
-# if intNum > 99:
-#     tens_digit = (intNum//10)%10
-# else:
-#     tens_digit = intNum//10
-
+# tens_digit = intNum//10
 # ones_digit = intNum%10
-# actual_digit = (f"{tens_digit}{ones_digit}")
-# print(hundred_digit)
-# print(tens_digit)
-# print(ones_digit)
-# # print(f"{tens_digit}{ones_digit}")
-# # print(actual_digit)
+# # print(tens_digit)
+# # print(ones_digit)
 
-# while intNum > 1000 or intNum < 0:
+
+# while intNum > 99 or intNum < 0:
 #         print(f"Number is out of range")
 #         exit()
 
@@ -168,10 +80,86 @@ while True:
 #     elif intNum in tensNumberList:
 #         print(str(tensNumberList[intNum]))
 #         break
-#     elif intNum not in onesNumberList and intNum not in tensNumberList and intNum < 99:
+#     elif intNum not in onesNumberList and intNum not in tensNumberList:
 #         # wordNum = tensNumberList[tens_digit*10] + onesNumberList[ones_digit*10]
 #         print(str(tensNumberList[tens_digit*10] + "-" + onesNumberList[ones_digit]))
 #         break
-#     else:
-#         print(str(HundredNumberList[hundred_digit*100] + (tensNumberList[tens_digit*10] + "-" + onesNumberList[ones_digit])))
-#         break
+ 
+#=======================================
+#Version2
+#=======================================
+
+NumberList = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine", 
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "ninteen",
+    20: "twenty",
+    30: "thirty",
+    40: "fory",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninty",
+    100: "one hundred",
+    200: "two hundred",
+    300: "three hundred",
+    400: "four hundred",
+    500: "five hundred",
+    600: "six hundred",
+    700: "seven hundred",
+    800: "eight hundred",
+    900: "nine hundred", 
+}
+
+pickANumber = input("Pick a number between 0 and 99: ")
+intNum = int(pickANumber)
+#========
+#digits
+#========
+hundred_digit = intNum//100
+
+if intNum > 99:
+    tens_digit = intNum//10
+    tens_digit_mod = tens_digit%10
+else:
+    tens_digit = intNum//10
+
+ones_digit = intNum%10
+#========
+print(hundred_digit)
+# print(tens_digit)
+print(tens_digit_mod)
+print(ones_digit)
+
+
+while intNum >= 1000 or intNum < 0:
+        print(f"Number is out of range")
+        exit()   
+while True:
+    if intNum in NumberList:
+        print(str(NumberList[intNum]))
+        break
+    elif intNum not in NumberList and intNum < 99: 
+        print(str(NumberList[tens_digit*10] + "-" + NumberList[ones_digit]))
+        break
+    else:
+        print(str(NumberList[hundred_digit*100] + " " + (NumberList[tens_digit_mod*10] + "-" + NumberList[ones_digit])))
+        break
---
+++ code/daniel/04_BlackJackAdvice/daniel-Lab4-Blackjack Advice.py
@@ -0,0 +1,72 @@
+# Lab 4: Blackjack Advice
+
+# Let's write a python program to give basic blackjack playing advice during a game by asking the player for cards. 
+# First, ask the user for three playing cards (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K). 
+# Then, figure out the point value of each card individually. Number cards are worth their number, all face cards are worth 10. 
+# At this point, assume aces are worth 1. Use the following rules to determine the advice:
+
+#     Less than 17, advise to "Hit"
+#     Greater than or equal to 17, but less than 21, advise to "Stay"
+#     Exactly 21, advise "Blackjack!"
+#     Over 21, advise "Already Busted"
+
+# Print out the current total point value and the advice.
+
+# What's your first card? Q
+# What's your second card? 2
+# What's your third card? 3
+# 15 Hit
+
+# What's your first card? K
+# What's your second card? 5
+# What's your third card? 5
+# 20 Stay
+
+# What's your first card? Q
+# What's your second card? J
+# What's your third card? A
+# 21 Blackjack!
+#=============================================================
+
+import random
+
+Facecards = {
+    "K": 10,
+    "Q": 10,
+    "J": 10,
+    "10": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "A": 1
+}
+
+Question1 = input("What's your first card?: ")
+Question2 = input("What's your second card?: ")
+Question3 = input("What's your third card?: ")
+
+total = int(Question1) + int(Question2) + int(Question3)
+
+
+
+if total < 17:
+    print("Hit")
+elif total <= 17 and total < 21:
+    print("Stay")
+elif total == 21:
+    print("BlackJack!")
+else:
+    print("Busted")
+
+
+
+# Version 2 (optional)
+
+# Aces can be worth 11 if they won't put the total point value of both cards over 21. Remember that you can have multiple aces in a hand. 
+# Try generating a list of all possible hand values by doubling the number of values in the output whenever you encounter an ace. 
+# For one half, add 1, for the other, add 11. This ensures if you have multiple aces that you account for the full range of possible values.
