25538480518d30dd12342d84eade67552a0ffa60
sushi <dandouglass14@gmail.com>
2022-07-11 20:23:01 -0400

    Lab3-daniel-Final

---
+++ code/daniel/05_Pick6/daniel-Lab5_Pick6.py
@@ -0,0 +1,76 @@
+# Lab 5: Pick6
+
+# Have the computer play pick6 many times and determine net balance.
+
+# Initially the program will pick 6 random numbers as the 'winner'. Then try playing pick6 100,000 times, with the ticket cost and payoff below.
+
+# A ticket contains 6 numbers, 1 to 99, and the number of matches between the ticket and the winning numbers determines the payoff. 
+# Order matters, if the winning numbers are [5, 10] and your ticket numbers are [10, 5] you have 0 matches. 
+# If the winning numbers are [5, 10, 2] and your ticket numbers are [10, 5, 2], you have 1 match. Calculate your net winnings (the sum of all expenses and earnings).
+
+#     a ticket costs $2
+#     if 1 number matches, you win $4
+#     if 2 numbers match, you win $7
+#     if 3 numbers match, you win $100
+#     if 4 numbers match, you win $50,000
+#     if 5 numbers match, you win $1,000,000
+#     if 6 numbers match, you win $25,000,000
+
+# One function you might write is pick6() which will generate a list of 6 random numbers, which can then be used for both the winning numbers and tickets. 
+# Another function could be num_matches(winning, ticket) which returns the number of matches between the winning numbers and the ticket.
+# Steps
+
+#     Generate a list of 6 random numbers representing the winning tickets
+#     Start your balance at 0
+#     Loop 100,000 times, for each loop:
+#     Generate a list of 6 random numbers representing the ticket
+#     Subtract 2 from your balance (you bought a ticket)
+#     Find how many numbers match
+#     Add to your balance the winnings from your matches
+#     After the loop, print the final balance
+
+# Version 2
+
+# The ROI (return on investment) is defined as (earnings - expenses)/expenses. Calculate your ROI, print it out along with your earnings and expenses.
+# ====================================================================================================================================================
+
+import random
+
+def pick6():
+    numlist = []
+    for r in range(6):
+        x = random.randint(1,99)
+        numlist.append(x)
+    print(numlist)
+
+pick6()
+pick6()
+#==============================
+
+# simulations = 100,000
+# possible_values = list(range(0,6))
+# # print(possible_values)
+
+# outcomes = {i: 0 for i in possible_values}
+
+# for i in range(simulations):
+#     d1 = random.randint(0,99)
+#     d2 = random.randint(0,99)
+
+#     outcome = d1 + d2
+#     outcomes[outcome] += 1
+
+# print(outcomes)
+
+#=================================
+
+
+
+
+
+
+
+
+
+
+
