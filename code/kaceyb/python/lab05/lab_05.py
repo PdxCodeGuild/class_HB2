e3d8783e6e14590425d186152bc618d51224fdd5
Kacey Broetje <kaceylauinger@gmail.com>
2022-07-11 17:20:52 -0700

    ran formatter

--- code/kaceyb/python/lab05/lab_05.py
+++ code/kaceyb/python/lab05/lab_05.py
@@ -8,7 +8,7 @@
 # file.write(message + '\n')
 # file.close()
 
-# try: 
+# try:
 #     num = int(input('Enter a number: '))
 #     divide_num = 100/num
 # except (ValueError, ZeroDivisionError):
@@ -45,9 +45,8 @@
 #         else:
 #             print('Pleaes enter a valid number')
 #         # print(error, 'Printing except error')
-        
-# print('No error has occured')
 
+# print('No error has occured')
 
 
 # Lab 5: Pick6
@@ -56,8 +55,8 @@
 
 # Initially the program will pick 6 random numbers as the 'winner'. Then try playing pick6 100,000 times, with the ticket cost and payoff below.
 
-# A ticket contains 6 numbers, 1 to 99, and the number of matches between the ticket and the winning numbers determines the payoff. 
-# Order matters, if the winning numbers are [5, 10] and your ticket numbers are [10, 5] you have 0 matches. 
+# A ticket contains 6 numbers, 1 to 99, and the number of matches between the ticket and the winning numbers determines the payoff.
+# Order matters, if the winning numbers are [5, 10] and your ticket numbers are [10, 5] you have 0 matches.
 # If the winning numbers are [5, 10, 2] and your ticket numbers are [10, 5, 2], you have 1 match. Calculate your net winnings (the sum of all expenses and earnings).
 
 #     a ticket costs $2
@@ -68,7 +67,7 @@
 #     if 5 numbers match, you win $1,000,000
 #     if 6 numbers match, you win $25,000,000
 
-# One function you might write is pick6() which will generate a list of 6 random numbers, which can then be used for both the winning numbers and tickets. 
+# One function you might write is pick6() which will generate a list of 6 random numbers, which can then be used for both the winning numbers and tickets.
 # Another function could be num_matches(winning, ticket) which returns the number of matches between the winning numbers and the ticket.
 # Steps
 
@@ -90,17 +89,18 @@ def num_matches(winning, ticket):
     matches = 0
     for number in range(6):
         # print(winning[number], ticket[number])
-        
-        
+
         if winning[number] == ticket[number]:
             # print("YOU WIN!")
-            matches += 1       
+            matches += 1
     return matches
+
 
 # Generate a list of 6 random numbers representing the winning tickets
 
+
 def pick6():
-    ticket_numbers = [] 
+    ticket_numbers = []
     for x in range(6):
         # print(x)
         ticket_numbers.append(random.randint(1, 99))
@@ -111,15 +111,14 @@ def pick6():
 winning_ticket = pick6()
 earnings = 0
 runs = 100000
-expenses = runs*2
+expenses = runs * 2
 
 for x in range(runs):
 
     match_counter = 0
 
     match_counter += num_matches(winning_ticket, pick6())
-    
-    
+
     if match_counter == 1:
         earnings += 4
     elif match_counter == 2:
@@ -132,43 +131,16 @@ for x in range(runs):
         earnings += 1000000
     elif match_counter == 6:
         earnings += 25000000
-    
+
 payout = earnings - expenses
 
 print(payout)
 
 
-
 #        VERSION 2        #
-    
 
-roi = (earnings - expenses)/expenses
 
+roi = (earnings - expenses) / expenses
 
 
 print(roi)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
