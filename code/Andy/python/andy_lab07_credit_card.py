5d24abd99da076b3af0a77a0de703f4bb5a2f535
andy2124 <andygarcia22212@gmail.com>
2022-07-15 20:52:51 -0700

    still working on

--- code/Andy/python/andy_lab07_credit_card.py
+++ code/Andy/python/andy_lab07_credit_card.py
@@ -1,1 +1,9 @@
+def credit_card_validator(card):
+    temp_list = []
+    for i in card:
+        temp_list.append(int(i))
+        
+    # check_digit = [":-1"]#list comprehension 
 
+card = "4556737586899855"
+print(card)
