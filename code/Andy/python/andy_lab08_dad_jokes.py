dd08b61d340f102a86c1f7ccdc4adddee3720539
andy2124 <andygarcia22212@gmail.com>
2022-07-15 19:56:23 -0700

    still working on

---
+++ code/Andy/python/andy_lab08_dad_jokes.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'https://icanhazdadjoke.com/'
+response = requests.get("https://icanhazdadjoke.com", headers={
+    'Accept': 'application/json'
+})
+print(response)
+data = response.json()
+
+print(data)
