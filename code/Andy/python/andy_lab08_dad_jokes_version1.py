77c176d5c86e2e7b8bcafab90eedf8f9d89f6a06
andy2124 <andygarcia22212@gmail.com>
2022-07-16 19:46:12 -0700

    almost done

--- code/Andy/python/andy_lab08_dad_jokes_version1.py
+++ code/Andy/python/andy_lab08_dad_jokes_version1.py
@@ -4,7 +4,12 @@ url = 'https://icanhazdadjoke.com/'
 response = requests.get("https://icanhazdadjoke.com", headers={
     'Accept': 'application/json'
 })
-print(response)
+# print(response)
 data = response.json()
+joke = input('want to hear a joke? y or n: ')
+if joke == 'y':
+    print(data['joke'])
+ 
 
-print(data['joke'])
+
+
