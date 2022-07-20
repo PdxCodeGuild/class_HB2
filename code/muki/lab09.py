1bfa717aa3457ab33a6fab820bf15ba99f4b7a2a
Muki-has-ideas <willshelnutt@gmail.com>
2022-07-19 21:37:32 -0700

    https://github.com/PdxCodeGuild/class_HB2.git

--- code/muki/lab09.py
+++ code/muki/lab09.py
@@ -9,7 +9,7 @@ import requests
 import time
 # url i is gonna use https://favqs.com/api/qotd
 
-question = input("Would you like a quote? \ny/n\n>")
+question = input("Would you like a quote? \ny/n\n\t> ")
 if question == "y":
     response = requests.get('https://favqs.com/api/qotd')
     # print(response)
@@ -24,7 +24,24 @@ if question == "y":
     time.sleep(2)
 elif question != "y":
     time.sleep(1)
-    print("Well, I didn't have one anyway. Goodbye\n\n")
+    # print("Well, I didn't have one anyway. Goodbye\n\n")
+    print("Well, I didn't have one anyway.\n\n")
+time.sleep(1)    
+#added a delay and onto version 2
+'''
+q2 = input('Enter a keyword to search for a quotes:\n\t>')
+response = requests.get('https://favqs.com/api/qotd')
+# print(response)
+q = response.text
+# print(q)
+q = response.json()
+print(' ')
+quote = q['quote']
+print(f'\n\n\n\n\n\n"{quote["body"]}" \n\n\t\t')
+time.sleep(1)
+print(f'-- {quote["author"]}\n\n\n\n\n\n\n\n')
+time.sleep(2)
+'''
 
 
 
