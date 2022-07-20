5402140d259f3b669b5a8828693ea80b4ec2fe81
Matt Nichols <mudbotmatt@gmail.com>
2022-07-19 18:31:04 -0700

    Lab08, still working

---
+++ code/matt/lab08-requests.py
@@ -0,0 +1,29 @@
+response = requests.get("https://icanhazdadjoke.com", headers={
+    'Accept': 'application/json'
+})
+
+print(response.text)
+# joke = response.json()
+# print(joke['joke'])
+
+#version 1
+joke = response.json()
+search_term = input("Input a genre for a joke you'd like to see ")
+
+#Think about how to do this.. First we need to figure out how to pull a singular joke. secondly
+
+response = requests.get(f"https://icanhazdadjoke.com/search?term=${search_term}", headers={
+    'Accept': 'application/json'
+})
+print(response.text)
+joke = response.json()
+first_step = (joke['results'])
+second_step = (first_step[0]['joke'])
+print(second_step)
+for key in range (len(first_step)):
+    print(key, first_step[key])
+
+# joke = response.json()
+# search_term = joke['golf']
+# print(response.json())
+# joke = response.json()
