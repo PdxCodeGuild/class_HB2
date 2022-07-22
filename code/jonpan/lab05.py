8e12fe362e3f630ad207dc73995e8fd09602ea2a
jonpan <jonathan.pan@gmail.com>
2022-07-21 19:07:21 -0700

    resubmitting lab05

--- code/jonpan/lab05.py
+++ code/jonpan/lab05.py
@@ -12,7 +12,7 @@ def pick_number(x=6):
 
 balance = 0
 loops = 100000
-ticket_cost = (loops * -2)
+expenses = (loops * -2)
 winning_ticket = pick_number()
 
 for x in range(loops):
@@ -32,11 +32,11 @@ for x in range(loops):
     elif match_counter == 6:
         balance += 25000000
 
-payoff = balance + ticket_cost
-ROI = (payoff - ticket_cost) / payoff 
+earnings = balance + expenses
+ROI = (earnings - expenses) / earnings
 # ROI_pct = "{. 0%}".format(ROI)
 # print(ROI_pct)
 
-print(f"\nPayoff: {payoff} ")
-print(f"\nExpenses: {ticket_cost} ")
+print(f"\nEarnings: {earnings} ")
+print(f"\nExpenses: {expenses} ")
 print(f"\nROI: {ROI:.0%} ")
