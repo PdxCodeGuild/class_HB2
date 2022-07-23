aaf30af0cc0664f95dc27ba66e4ec195c385cdcc
sushi <dandouglass14@gmail.com>
2022-07-22 23:08:48 -0400

    lab12 in progress

--- code/daniel/lab12_atm/daniel_lab12_atm .py
+++ code/daniel/lab12_atm/daniel_lab12_atm .py
@@ -20,7 +20,7 @@
 #================================================
 
 class ATM:
-    def __init__(self, balance = 0, interest = .1):
+    def __init__(self, balance = 0, interest = .001):
         self.balance = balance
         self.interest = interest
 
@@ -31,19 +31,24 @@ class ATM:
     def deposit(self, amount):
         #  deposits the given amount in the account
         self.balance += amount
+        return self.balance
         
     def check_withdraw(self, amount):
         # returns true if the withdrawn amount won't put the account in the negative
-        if amount <= self.balance:
-            self.balance -= amount
+        if self.balance < amount:
+            return False
         else:
-            return balance
+            return True
     def withdraw(self, amount):
         # withdraws the amount from the account and returns it
         self.balance -= amount
 
 
-    # def calc_interest(self):
+    def calc_interest(self):
+        # returns the amount of interest calculated on the account
+        
+        
+
 
 
 
@@ -62,16 +67,16 @@ while True:
         atm.deposit(cat) # call the deposit(amount) method
         print(f'Deposited ${cat}')
     elif command == 'withdraw':
-        amount = float(input('How much would you like '))
+        amount = float(input('How much would you like? '))
         if atm.check_withdraw(amount): # call the check_withdrawal(amount) method
             atm.withdraw(amount) # call the withdraw(amount) method
             print(f'Withdrew ${amount}')
         else:
             print('Insufficient funds')
-    # elif command == 'interest':
-    #     amount = atm.calc_interest() # call the calc_interest() method
-    #     atm.deposit(amount)
-    #     print(f'Accumulated ${amount} in interest')
+    elif command == 'interest':
+        amount = atm.calc_interest() # call the calc_interest() method
+        atm.deposit(amount)
+        print(f'Accumulated ${amount} in interest')
     # elif command == 'help':
     #     print('Available commands:')
     #     print('balance  - get the current balance')
@@ -81,5 +86,5 @@ while True:
     #     print('exit     - exit the program')
     # elif command == 'exit':
     #     break
-    else:
-        print('Command not recognized')
+    # else:
+    #     print('Command not recognized')
