e1088f4f42480c16cd53ab8c441471c8b833f691
andy2124 <andygarcia22212@gmail.com>
2022-07-22 18:36:02 -0700

    still working on

---
+++ code/Andy/python/andy_lab10_atm_v1.py
@@ -0,0 +1,47 @@
+class Atm:
+    def __init__(self, balance, intrest_rate):
+        self.bal= 0
+        self.balance = balance 
+        self.intrest_rate= intrest_rate
+
+    def check_balance():
+
+
+
+
+
+
+
+# atm = ATM() # create an instance of our class
+# print('Welcome to the ATM')
+# while True:
+#     command = input('Enter a command: ')
+#     if command == 'balance':
+#         balance = atm.check_balance() # call the check_balance() method
+#         print(f'Your balance is ${balance}')
+#     elif command == 'deposit':
+#         amount = float(input('How much would you like to deposit? '))
+#         atm.deposit(amount) # call the deposit(amount) method
+#         print(f'Deposited ${amount}')
+#     elif command == 'withdraw':
+#         amount = float(input('How much would you like '))
+#         if atm.check_withdrawal(amount): # call the check_withdrawal(amount) method
+#             atm.withdraw(amount) # call the withdraw(amount) method
+#             print(f'Withdrew ${amount}')
+#         else:
+#             print('Insufficient funds')
+#     elif command == 'interest':
+#         amount = atm.calc_interest() # call the calc_interest() method
+#         atm.deposit(amount)
+#         print(f'Accumulated ${amount} in interest')
+#     elif command == 'help':
+#         print('Available commands:')
+#         print('balance  - get the current balance')
+#         print('deposit  - deposit money')
+#         print('withdraw - withdraw money')
+#         print('interest - accumulate interest')
+#         print('exit     - exit the program')
+#     elif command == 'exit':
+#         break
+#     else:
+#         print('Command not recognized')
