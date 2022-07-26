901d7d04f81e919312c6687785948e501f3cba87
sushi <dandouglass14@gmail.com>
2022-07-25 21:00:42 -0400

    lab8 finished

--- code/daniel/lab08-dad_api/lab08-dad_api.py
+++ code/daniel/lab08-dad_api/lab08-dad_api.py
@@ -16,35 +16,69 @@
 # response = requests.get(f"https://icanhazdadjoke.com/search?term=${search_term}", headers={
 #     'Accept': 'application/json'
 # })
-# ======================================================================================================
 
-import time
+#====================================
+
+# print(response.text) # {"ip":"76.105.187.182"} - raw json response
+# data = response.json() # turn raw json into a python dictionary
+# print(data) # {'ip': '76.105.187.182'} - python dictionary
+# print(data['ip']) # 76.105.187.182
+
+
+#=======================================================
+# Version1
+#=======================================================
+
+# import requests
+# import time
+
+# response = requests.get("https://icanhazdadjoke.com", headers={'Accept': 'application/json'})
+
+
+# # print(response.text)
+# joke = response.json()
+# # print(joke)
+
+# dad_joke = input("Do you wank to hear a dad joke? y or n?: ")
+
+# while True:
+#     if dad_joke == "y" or dad_joke == "yes":
+#         print(joke['joke']) 
+#         break
+#     elif dad_joke == "n" or dad_joke == "no":
+#         print("goodbye")
+#         break
+#     else:
+#         print("Not a valid answer")
+#         break
+
+
+
+#=======================================================
+# Version2
+#=======================================================
+
 import requests
+import time
 
-response = requests.get("https://icanhazdadjoke.com", headers={'Accept': 'application/json'})
+
+search_joke = input("Enter a search term to find a related dad joke: ")
+# print(search_joke)
+
+response = requests.get(f"https://icanhazdadjoke.com/search?term=${search_joke}", headers={'Accept': 'application/json'})
+# print(response)
+
+
+
 joke = response.json()
+jokes = joke["results"]
 
-# print(response.url)
-# print(response.status_code)
-# print(response.encoding)
-# print(response.headers)
-# print(response.text)
-# print(response.json())
-
-print(joke)
-
-data = response.json()
-# print(data[0]['title'])
-for film in data:
-    print(film['title'])
-    print(film['release_date'])
-    print(film['description'])
-    print('-'*10)
-
-#========================================
-
-# Add the ability to "search" for jokes using another endpoint. Create a REPL that allows one to enter a search term and go through jokes one at a time.
-
-
-# response = requests.get(f"https://icanhazdadjoke.com/search?term=${search_term}", headers={'Accept': 'application/json', params=query)})
-
+for joke in jokes:
+    print(joke["joke"])
+    more_jokes = input("Do you want to hear another dad joke? yes/no: ")
+    if more_jokes == "yes" or more_jokes == "y":
+        print(joke["joke"])
+    elif more_jokes == "no" or more_jokes == "n":
+        break
+    else:
+        print("Not a valid answer")
