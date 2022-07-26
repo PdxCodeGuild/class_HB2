8c33f557f93c061678066479d0c9ee2440dda2f2
Zachary Christman <zacharywchristman2@outlook.com>
2022-07-25 22:26:12 -0400

    Committing initial files for lab 11.

---
+++ code/zach/lab11-contact-list.py
@@ -0,0 +1,17 @@
+def version1():
+    with open('contacts.csv','r') as file:
+        lines = file.read().split('\n')
+        print(lines)
+
+def version2():
+    
+    return 0
+
+def version3():
+    
+    return 0
+
+def main():
+    version1()
+
+main()
