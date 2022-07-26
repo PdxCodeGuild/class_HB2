75061c3669d1ea91a393e01c942fe586a76a15f7
Zachary Christman <zacharywchristman2@outlook.com>
2022-07-26 00:39:26 -0400

    Adding progress on lab 2. Currently working on version 2.

--- code/zach/lab11-contact-list.py
+++ code/zach/lab11-contact-list.py
@@ -1,25 +1,52 @@
 def version1():
     with open('contacts.csv', 'r') as file:
         lines = file.read().split('\n')
-        #print(lines)
-    
+    contacts = []
     index = 1
     headers = lines[0].split(",")
     
     while index < len(lines):
         contact = lines[index].split(",")
-        print(dict(zip(headers, contact)))
+        contacts.append(dict(zip(headers, contact)))
         index += 1
+    
+    return contacts
 
 def version2():
     
-    return 0
+    query_type = input("What would you like to do?\nCreate\nRetrieve\nUpdate\nDelete\n> ")
 
+    if query_type == 'Create':
+        with open('contacts.csv', 'a') as file:
+            name = input("What is the contact's name? ")
+            phone = input("What is the contact's phone? ")
+            email = input("What is the contact's email? ")
+            file.write(f'\n{name}, {phone}, {email}')
+            
+    elif query_type == 'Retrieve':
+        contacts = version1()
+        name = input("What is the contact's name? ")
+        index = 0 
+
+        while index < len(contacts):
+            if name == contacts[index]['Name']:
+                return print(contacts[index])
+            index += 1
+        else:
+            return print("User not found.")
+    
+    #elif query_type == 'Update':
+        
+    #elif query_type == 'Delete':
+        
+    #else:
+    
 def version3():
     
     return 0
 
 def main():
-    version1()
+    #version1()
+    version2()
 
 main()
