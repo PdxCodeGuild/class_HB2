2e3db22f94a5334a469eadcc48875a15b5eacbf7
andy2124 <andygarcia22212@gmail.com>
2022-07-25 20:44:32 -0700

    still working on

--- code/Andy/python/andy_lab11_contact_list_v1.py
+++ code/Andy/python/andy_lab11_contact_list_v1.py
@@ -1,3 +1,12 @@
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
-    print(lines)
+    print(lines, 'lines')
+
+
+headers = lines[0].split(' , ') #getting the keys by themselves
+print('headers: ', headers)
+
+answer = lines[1:]
+print('answers: ', answer)
+# list = {}
+# for line in lines:
