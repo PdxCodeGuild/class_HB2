631da203c3b3546886ed02c35e7204c9a8ba9ccd
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-07-27 22:11:12 -0700

    Lab

--- code/kelin/labs/lab05_pick6.py
+++ code/kelin/labs/lab05_pick6.py
@@ -2,48 +2,48 @@
 
 # Have the computer play pick6 many times and determine net balance.
 
-import random
+# import random
 
-def pick6game():
-    numbers = [] #something to put the nums in 
-    for i in range(1,7): #Should run the loop 6 times 
-        numbers.append(random.randint (1,99))
-    return numbers
+# def pick6game():
+#     numbers = [] #something to put the nums in 
+#     for i in range(1,7): #Should run the loop 6 times 
+#         numbers.append(random.randint (1,99))
+#     return numbers
 
 
-def compare(winning_ticket, current_ticket):
-    counter = 0 #keeping track of how many matches  
-    for i in range(6): #checking the index of the two arguments range is returning an array which starts at the index of 0 and then goes up to 5
-        if winning_ticket[i] == current_ticket[i]:# running the 6 times 
-            counter +=1 # counting how many match 
-    return counter 
+# def compare(winning_ticket, current_ticket):
+#     counter = 0 #keeping track of how many matches  
+#     for i in range(6): #checking the index of the two arguments range is returning an array which starts at the index of 0 and then goes up to 5
+#         if winning_ticket[i] == current_ticket[i]:# running the 6 times 
+#             counter +=1 # counting how many match 
+#     return counter 
     
 
-# print(compare(number_picker(), number_picker())) 
-                                                #test
-winner = number_picker() # winning ticket on the outside so it stays constant
-balance = 0 # Holder
+# # print(compare(number_picker(), number_picker())) 
+#                                                 #test
+# winner = number_picker() # winning ticket on the outside so it stays constant
+# balance = 0 # Holder
 
-for i in range(100000):
-    comparing_tickets = compare(number_picker(), winner) #i'm getting my compare function putting my number_picker funtion with in it which is the one thats gonna be getting changed the 100,000 time and comparing it to the winner which is the one that will be staying the same all those times 
-    balance -= 2  # since the balnce on top is the holder im making it -2 since that is how much it is to pay for a ticket 
-    if comparing_tickets == 1: # if one of the compared matches has one match in the same spot we get $2 since we were already losing 2
-        balance += 4
-    elif comparing_tickets == 2:
-        balance += 7 
-    elif comparing_tickets == 3:
-        balance += 100
-    elif comparing_tickets == 4:
-        balance += 50,000
-    elif comparing_tickets == 5:
-        balance += 1,000,000
-    elif comparing_tickets == 6:
-        balance += 25,000,000
+# for i in range(100000):
+#     comparing_tickets = compare(number_picker(), winner) #i'm getting my compare function putting my number_picker funtion with in it which is the one thats gonna be getting changed the 100,000 time and comparing it to the winner which is the one that will be staying the same all those times 
+#     balance -= 2  # since the balnce on top is the holder im making it -2 since that is how much it is to pay for a ticket 
+#     if comparing_tickets == 1: # if one of the compared matches has one match in the same spot we get $2 since we were already losing 2
+#         balance += 4
+#     elif comparing_tickets == 2:
+#         balance += 7 
+#     elif comparing_tickets == 3:
+#         balance += 100
+#     elif comparing_tickets == 4:
+#         balance += 50,000
+#     elif comparing_tickets == 5:
+#         balance += 1,000,000
+#     elif comparing_tickets == 6:
+#         balance += 25,000,000
 
-return_of_in = balance / 200000 # getting the balance and then dividing it bt the 200,000 because thats the two dollars to buy the ticket and multiplying it by the many times we're gonna but the ticket 
-print(f'The winning lotto numbers are {winner}') 
-print(f'\n and your balance is {balance}')
-print(f'Your ROI is {return_of_in}')
+# return_of_in = balance / 200000 # getting the balance and then dividing it bt the 200,000 because thats the two dollars to buy the ticket and multiplying it by the many times we're gonna but the ticket 
+# print(f'The winning lotto numbers are {winner}') 
+# print(f'\n and your balance is {balance}')
+# print(f'Your ROI is {return_of_in}')
 
 
    
