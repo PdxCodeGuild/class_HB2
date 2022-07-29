9d6cd52d1ee45bfa6a20d4b7fc9a43e1dd53b962
Muki-has-ideas <willshelnutt@gmail.com>
2022-07-28 19:24:56 -0700

    commit cause i am gonna take a break

---
+++ code/muki/minicap.py
@@ -0,0 +1,14 @@
+import turtle
+import random 
+
+
+# gonna try to go for a "spy hunter" or "avoid the obstacle" style games
+
+w = 500 # this is the width of the window in which the game will be played.  
+h = 500 # this is the height of the play window
+o = 10 # this is the obstacle size 
+
+c_sides = ("green")
+c_road = ("black")
+
+block_size = 5 # this will be the amount that the screen is moved while running
