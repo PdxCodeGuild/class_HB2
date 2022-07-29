889a1ed4672c12f079e59f36fec4eb504b350479
Zachary Christman <zacharywchristman2@outlook.com>
2022-07-29 01:05:37 -0400

    Completed the get weather API call and added a temp converter.

--- code/zach/mini-capstone.py
+++ code/zach/mini-capstone.py
@@ -1,7 +1,6 @@
-from authlib.jose import jwt # https://docs.authlib.org/en/latest/jose/index.html
+import jwt
 import time # https://www.systutorials.com/how-to-get-the-epoch-timestamp-in-python/
 import requests
-
 
 def create_key(signature_alg, key_id, team_id, service_id, iat, key_path):
     """This function takes the required information as arguments and returns an encoded key to pass as a parameter for the Apple WeatherKit API."""
@@ -9,7 +8,6 @@ def create_key(signature_alg, key_id, team_id, service_id, iat, key_path):
         key = f.read()
     
     header = {
-        "alg": signature_alg,
         "kid": key_id,
         "id": f"{team_id}.{service_id}",
     }
@@ -21,11 +19,12 @@ def create_key(signature_alg, key_id, team_id, service_id, iat, key_path):
         "sub": service_id
     }
 
-    s = jwt.encode(header, payload, key)
+    s = jwt.encode(payload, key, algorithm=signature_alg, headers=header)
 
     return s
 
 def get_weather(language, latitude, longitude, web_token, timezone):
+    """This function takes the location data, web token from create_key(), and the timezone to return the current weather condition and temperature from the Apple WeatherKit API."""
     response = requests.get(f"https://weatherkit.apple.com/api/v1/weather/{language}/{latitude}/{longitude}", headers = {
         'Authorization': f'Bearer {web_token}',
         'Accept': 'application/json'
@@ -34,9 +33,16 @@ def get_weather(language, latitude, longitude, web_token, timezone):
         'timezone': timezone,
         'dataSets': 'currentWeather'
     })
-    print(response)
-    #current_weather = response.json()
-    #return current_weather
+
+    weather = response.json()
+    
+    current_condition = weather['currentWeather']['conditionCode']
+    current_temp = weather['currentWeather']['temperature']
+    
+    return current_condition, current_temp
+
+def temp_converter(temp_celsius):
+    return temp_celsius * 1.8 + 32
     
 def main():
     web_token = create_key("ES256",
@@ -47,6 +53,9 @@ def main():
         "C:/Users/zacha/OneDrive/Coding/Keys/Home_Dashboard/WeatherKit/AuthKey_74B4NT7KNA.p8"
         )
 
-    get_weather('en', 38.933868, -77.177261, web_token, 'America/New_York')
+    current_condition, current_temp = get_weather('en', 38.933868, -77.177261, web_token, 'America/New_York')
+    temp_fahrenheit = temp_converter(current_temp)
+    
+    print(f'It is currently {current_condition} and {round(temp_fahrenheit, 2)} degrees Fahrenheit ({current_temp} degrees Celsius).')
     
 main()
