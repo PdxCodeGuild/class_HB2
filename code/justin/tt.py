d47384773ad6dfb053671b9b88fbfbc86097fe4a
justin-young5 <104773737+justin-young5@users.noreply.github.com>
2022-07-31 22:06:36 -0500

    Delete tt.py

--- code/justin/tt.py
+++
@@ -1,6 +0,0 @@
-with open('contacts.csv', 'r') as file:
-    lines = file.read().split('\n')
-    for num, line in enumerate(lines):
-        if num == 5:
-            print(line)
-        print(num, line)
