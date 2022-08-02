7c97765f2a3c836709d8521ff1065012ac1fd7d2
justin-young5 <justin.young5@yahoo.com>
2022-08-01 23:27:39 -0500

    trying to merge back on this branch

--- code/justin/files.py
+++ code/justin/files.py
@@ -8,7 +8,6 @@ dic={}
 p=1
 new_entry = []
 
-
 while True:
     print('''\nWelcome to our contact database, please select from the following options: 
 To create new user enter "create":
@@ -65,65 +64,46 @@ When finished, enter"done":''')
                         fin.close()
                     elif chg.lower() == 'phone':
                         chg = input('Whats the new phone? ')
-                        lines[1] = chg
+                        data = data.replace(lines[1], chg)
+                        fin.close()
+                        fin = open('contacts.csv', 'wt')
+                        fin.write(data)
+                        fin.close()
                     elif chg.lower() == 'state':
                         chg = input('Whats the new state? ')
-                        lines[2] = chg
+                        data = data.replace(lines[2], chg)
+                        fin.close()
+                        fin = open('contacts.csv', 'wt')
+                        fin.write(data)
+                        fin.close()
                     elif chg.lower() == 'spirit animal':
                         chg = input('Whats the new spirit animal? ')
-                        lines[3] = chg
+                        data = data.replace(lines[3], chg)
+                        fin.close()
+                        fin = open('contacts.csv', 'wt')
+                        fin.write(data)
+                        fin.close()
                     else:
                         print('not a valid option')
                     lines = (',').join(lines)
-                    
+    elif a.lower() == 'delete':
+        print('What contact do you want to delete? ')
+        n = input('Enter name: ')        
+        fin = open('contacts.csv', 'rt')
+        data = fin.read()
+        if n.lower() in data.lower():
+            print(f'User {n.title()} found')
+            for num, lines in enumerate(data.split('\n')):
+                print(num, lines)
+                if n.lower() in lines.lower():
+                    lines = lines.split(',')
+                    # lines = ','.join(lines)
+                    print(lines)
+                    chg = input(f'Is this the entry you wish to delete?(y/n) ')
 
 
-        # data = data.replace()
-        #     for l in lines:
-        #         if n.lower() in l.lower():  
-        #             print(f'Contact {l} found.')
-        #             spl = l.split(',')
-        #             upd = input('What needs updating, name, phone, state, spirit animal? ')
-        #             if upd.lower() == 'name'.lower():
-        #                 spl[0]
-                            
 
 
-#             print(spl)
-            # n = input('what name are we looking for: ')
-            # if n.lower() in l[0].lower():
-            #         spl = l.split(',')
-            #         spl_a = spl.copy()
-            #         print(f'{l} \nis the info on file, what needs to change:\nname,phone,state,spirit animal? ')
-            #         a = input('Enter what to change: ')
-            #         if a.lower() == 'name':
-            #             nchg = input('What do you want to change the name to? ')
-            #             print(spl_a)
-                        
-
-                    # if a.lower() == 'name':
-        
-# n = input('enter a name: ')
-# new_entry.append(n)
-# ph = input('enter a phone number: ')
-# new_entry.append(ph)
-# st = input(f'enter {n}\'s state: ')
-# new_entry.append(st)
-# sa = input(f'enter {n}\'s spirit animal: ')
-# new_entry.append(sa)
-# j = ','.join(new_entry)
-# with open('contacts.csv', 'a') as file:
-#         file.write(f'\n{j}') 
-# with open('contacts.csv', 'a') as file:
-#     new_entry = str(new_entry)
-#     contacts.csv.write(new_entry)
-
-# contacts=[]
-# keys=[]
-# ncontacts=[]
-# dic={}
-# p=1
-# new_entry = []
 
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
