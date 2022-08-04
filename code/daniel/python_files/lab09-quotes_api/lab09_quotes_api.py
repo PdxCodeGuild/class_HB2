eeea2073c151be049ebbbf98619085baf1cc09e5
sushi <dandouglass14@gmail.com>
2022-08-04 15:48:29 -0400

    fixed, loops to page 2

--- code/daniel/python_files/lab09-quotes_api/lab09_quotes_api.py
+++ code/daniel/python_files/lab09-quotes_api/lab09_quotes_api.py
@@ -25,12 +25,11 @@ url_format = "https://favqs.com/api/quotes?page={0}&filter={1}"
 # print(response.json())
 
 page = 1
+get_keyword = input("Enter a search term to get a related quote: ")
 
 
 while True:
-    get_keyword = input("Enter a search term to get a related quote: ")
     url_string = url_format.format(page, get_keyword)
-
     response = requests.get(url_string, headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
     print(response)
     keyword_quote = response.json()['quotes']
@@ -40,9 +39,11 @@ while True:
         print("======")
     next_response = input("Enter 'next page' or 'done': ")
     if next_response == 'next page':
-        page += 1
+            page += 1
     elif next_response == 'done':
-        break
+            break
+
+
 
 
 # ======================================================================
