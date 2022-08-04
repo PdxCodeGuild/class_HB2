b7ce188112e7e0027e8d2f6bc38da069f0fd96ff
sushi <dandouglass14@gmail.com>
2022-08-04 01:01:04 -0400

    lab09 re-upload

---
+++ code/daniel/python_files/lab09-quotes_api/lab09_quotes_api.py
@@ -0,0 +1,50 @@
+# https://favqs.com/api/qotd
+
+import requests
+#================================
+#Version1
+#================================
+
+
+# response = requests.get(f"https://favqs.com/api/qotd", headers={'Accept': 'application/json'})
+# # print(response.json())
+
+# random_quote = response.json()['quote']['body']
+# print(random_quote)
+
+# random_quote_author = response.json()['quote']['author']
+# print(' ~',random_quote_author)
+
+
+#================================
+#Version2
+#================================
+url_format = "https://favqs.com/api/quotes?page={0}&filter={1}"
+
+
+# print(response.json())
+
+page = 1
+
+
+while True:
+    get_keyword = input("Enter a search term to get a related quote: ")
+    url_string = url_format.format(page, get_keyword)
+
+    response = requests.get(url_string, headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+    print(response)
+    keyword_quote = response.json()['quotes']
+    print(len(keyword_quote))
+    for quote in keyword_quote:
+        print(quote['body'])
+        print("======")
+    next_response = input("Enter 'next page' or 'done': ")
+    if next_response == 'next page':
+        page += 1
+    elif next_response == 'done':
+        break
+
+
+# ======================================================================
+
+
