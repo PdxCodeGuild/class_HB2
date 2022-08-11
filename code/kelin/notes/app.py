256c6ba4c3e866b60312c51fde1248277f0adf41
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-08-10 19:46:29 -0700

    Added notes for Flask

--- code/kelin/notes/app.py
+++ code/kelin/notes/app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import code
+from flask import Flask, render_template, request
+app = Flask(__name__, template_folder='template')
+
+# @app.route('/')
+# def index():
+#     return 'Hello World!'
+
+# app.run(debug=True)
 
 @app.route('/')
-def index(username):
-  name = 'bob'
-  temperature = 67
-  nums = [1, 2, 3]  
-  return render_template('index.html', name=name, temperature=temperature, nums=nums)
+def index():
+    return render_template('index.html')
+
+@app.route('/sum')
+def addition():
+    num1= int(request.args['number1'])
+    num2= int(request.args['number2'])
+    algo = num1 + num2
+    return render_template('sum.html', code=algo)
+
+app.run(debug=True)
