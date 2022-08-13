87ad88bead9bef27f50b5c4dec5b7942ef61967f
sushi <dandouglass14@gmail.com>
2022-08-13 15:38:33 -0400

    lab complete

--- code/daniel/fhc_files/lab06-flask_redo/app.py
+++ code/daniel/fhc_files/lab06-flask_redo/app.py
@@ -1,7 +1,9 @@
 
-# ====
+#===========
 # app.py
-# ====
+#===========
+#Version1
+#===========
 
 from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='template')
@@ -18,42 +20,29 @@ def feet():
     return render_template('sum.html', code=ft_to_meters)
 
 
+#===========
+#Version2
+#===========
 
 
-# ===============
 @app.route('/convert')
 def convert():
     enter_unit_type = request.args['unit_type']
     enter_distance = int(request.args['distance'])
     converted = 0
     
-    ft_to_meters = float(enter_distance) * 0.3048
-    mi_to_meters = float(enter_distance) * 1609.34
-    m_to_meters = float(enter_distance)
-    km_to_meters = float(enter_distance) * 1000.00
-    yd_to_meters = float(enter_distance) * 0.9144
-    in_to_meters = float(enter_distance) * 0.0254
 
-    if enter_unit_type == "ft":
-        converted = str(enter_distance * ft_to_meters)
-        return converted
-    elif enter_unit_type == "mi":
-        converted = str(enter_distance * mi_to_meters)
-        return converted
-    elif enter_unit_type == "m":
-        converted = str(enter_distance * m_to_meters)
-        return converted
-    elif enter_unit_type == "km":
-        converted = str(enter_distance * km_to_meters)
-        return converted
-    elif enter_unit_type == "yd":
-        converted = str(enter_distance * yd_to_meters)
-        return converted
-    elif enter_unit_type == "in":
-        converted = str(enter_distance * in_to_meters)
-        return converted
-    else:
-        return ("Not a valid option")
+    unit_dict = {
+        "ft": 0.3048,
+        "mi": 1609.34,
+        "m": 1,
+        "km": 1000.00,
+        "yd": 0.9144,
+        "in": 0.0254
+    }
+
+    converted = enter_distance * unit_dict[enter_unit_type]
+
 
     return render_template('sum.html', code=converted)
     
