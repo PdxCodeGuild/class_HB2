8182309dc03429c42d7f1cb42ff96a8b7efb22ca
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-08-13 13:50:50 -0700

    Completed Lab 6 make change version 1 and 2

--- code/kelin/labs/lab06_make_change.py
+++ code/kelin/labs/lab06_make_change.py
@@ -8,6 +8,10 @@ import math
 
 ### Helped by T/A Grant
 
+# Version 2 (optional)
+
+# Instead of hard-coding the coins, store them in a list of tuples. This way you can make custom coins.
+
 coins = {
     'penny': 1,
     'nickel': 5,
@@ -17,27 +21,24 @@ coins = {
 
 print("Welcome to the Change Maker 5000 (tm)") # Welcome message
 
-total_amount = float(input("Enter a dollar amount: ")) # User enters a dollar amount
+total_amount = float(input("Enter a dollar amount: ")) # User enters a dollar amount and cents
+#  = float(i) # User input becomes a float
 
 total = total_amount * 100 # dollars input converter to pennies
 
+# Always break the total into the highest coin value first, resulting in the fewest amount of coins. For this, you'll have to use floor division //, 
+# which throws away the remainder. 10/3 is 3.333333, 10//3 is 3.
 
 quarters_n = total // coins['quarter'] # gets number of quarters
-
 leftover_1 = total % coins['quarter'] 
 
-
-dimes_n = total // coins['dime'] # gets number of quarters
-
+dimes_n = leftover_1 // coins['dime'] # gets number of quarters
 leftover_2 = leftover_1 % coins['dime'] 
 
-
 nickels_n = leftover_2 // coins['nickel'] # gets number of quarters
-
 leftover_3 = leftover_2 % coins['nickel'] 
 
-
-pennies_n = leftover_3 // coins['penny']
+pennies_n = leftover_3 // coins['penny'] # gets number of pennies
 
 quarters = int(quarters_n)
 dimes = int(dimes_n)
@@ -45,28 +46,12 @@ nickels = int(nickels_n)
 pennies = int(pennies_n)
 
 
-print(leftover_1, leftover_2, leftover_3, quarters, dimes, nickels, pennies)
+# print(leftover_1, leftover_2, leftover_3, quarters, dimes, nickels, pennies)
 
+print(f"You have {quarters} quarters, {dimes} dimes, {nickels} nickels, and {pennies} pennies.")
 
+# Prints number of coins in the dollar amount
 
-
-
-#  = int(i) # User input becomes an integer
-
-
-
-# Always break the total into the highest coin value first, resulting in the fewest amount of coins. For this, you'll have to use floor division //, 
-
-# which throws away the remainder. 10/3 is 3.333333, 10//3 is 3.
-
-# Version 2 (optional)
-
-# Instead of hard-coding the coins, store them in a list of tuples. This way you can make custom coins.
-
-# coins = [
-#     ('half-dollar', 50),
-#     ('quarter', 25),
-#     ('dime', 10),
-#     ('nickel', 5),
-#     ('penny', 1)
-# ]
+# Welcome to the Change Maker 5000 (tm)
+# Enter a dollar amount: 152.97
+# You have 611 quarters, 2 dimes, 0 nickels, and 2 pennies.
