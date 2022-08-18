056c73591c52ef4bf045cd9386b885127a517584
Zachary Christman <zachary.christman@8brconsulting.dev>
2022-08-17 22:19:47 -0400

    Setup folder for lab 6.

---
+++ code/zach/html-labs/lab06-flask/Example/flask-example.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="template")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sum')
+def addition():
+    num1 = int(request.args['number1'])
+    num2 = int(request.args['number2'])
+    algo = num1 + num2
+    return render_template('sum.html', code=algo)
+
+if __name__ == '__main__':
+    app.run(debug=True)
