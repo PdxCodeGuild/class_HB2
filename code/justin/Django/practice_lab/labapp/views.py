dc9adb4f8941be9551cbcc39c5049d7e0a18acc2
justin-young5 <104773737+justin-young5@users.noreply.github.com>
2022-08-23 22:14:34 -0500

    submitting lab

--- code/justin/Django/practice_lab/labapp/views.py
+++ code/justin/Django/practice_lab/labapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Priority, Todo
 from .forms import PriorityForm, TodoForm
 
@@ -24,14 +24,16 @@ def todo(request):
 
 def close(request):
     # todos = Todo.objects.all().values_list('item',flat=True)
-    todone = Todo.objects.all().values_list('completed_date',flat=True)
-    if 'item' in request.POST:
-        item = request.POST['work']
-    else: 
-        item = request.POST.get['work', False]
+    # todone = Todo.objects.all().values_list('completed_date',flat=True)
+    # thing = {'item':request.POST}
+    # done_item = Todo.objects.get(thing)
+    # context = {'done_item':done_item}
+    context = request.POST
+
+    return HttpResponse(context)
 
 
-    return render(request, 'labapp/close.html', {'item':item})
+    # return render(request, 'labapp/close.html', context)
 
 
 # def item(request):
