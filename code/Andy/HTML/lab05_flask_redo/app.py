6d507c479f254b998b95b01d3f1b206b1ec3abe6
andy2124 <andygarcia22212@gmail.com>
2022-08-24 16:27:36 -0700

    still working

--- code/Andy/HTML/lab05_flask_redo/app.py
+++ code/Andy/HTML/lab05_flask_redo/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from requests import post
 
 app = Flask(__name__, template_folder='template')
 
@@ -12,3 +13,22 @@ def index():
 
 @app.route('/recieve_form/', methods=['GET','POST'])
 def converter():
+    if request.method == 'POST':
+        distance = request.form['unit_distance']
+        in_units = request.form['input_units']
+        out_units = request.form['output_units']
+
+
+        measurments = {
+            'ft': 0.3048,
+            'mi': 1609.34,
+            'm': 1,
+            'km': 1000,
+            'yd': 0.9144,
+            'in': 0.0254,
+
+        }
+
+        in_results = measurments[in_units]
+        results = 
+
