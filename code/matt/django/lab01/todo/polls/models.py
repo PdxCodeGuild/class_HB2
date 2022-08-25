87243a7b10df35c2c0af4f5b9ad4d3ef08e66e99
Matt Nichols <mudbotmatt@gmail.com>
2022-08-24 21:48:01 -0700

    redo

--- code/matt/django/lab01/todo/polls/models.py
+++ code/matt/django/lab01/todo/polls/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 
 class Priority(models.Model):
-    name = models.CharField(max_length=200)
+    PRIORITY_NUMS = [
+        ('low', 'Low'),
+        ('med', 'Medium'),
+        ('high', 'High'),
+        ('imp', 'MOST IMPORTANT')
+    ]
+    name = models.CharField(max_length=200, choices=PRIORITY_NUMS, default="low", null=True)
+    
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
 class TodoItem(models.Model):
-    text = models.CharField(max_length=200)
-    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now=True)
+    text = models.CharField(max_length=200, null=True)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.text
--- code/matt/django/lab01/todo/polls/views.py
+++ code/matt/django/lab01/todo/polls/views.py
@@ -15,11 +15,11 @@ def todoitem_form(request):
     return render(request, 'polls/todoitem_form.html', context)
 
 def save_todo_item(request):
-    todo = request.POST.get("tditem")
+    todo = request.POST.get("tditem")#
     priority_id = request.POST.get('priority')
-    priority = Priority.objects.get(id=priority_id)
-    new_todoitem = TodoItem(text=todo, priority=priority)
-    new_todoitem.save()
+
+    TodoItem.objects.create(text=todo)
+    Priority.objects.create(name=priority_id)
     return HttpResponseRedirect('/polls/')
 
 
