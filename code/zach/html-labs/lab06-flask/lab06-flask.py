6fa7b1d366071b2500b32625551470f169fb3a7d
Zachary Christman <zachary.christman@8brconsulting.dev>
2022-08-24 23:51:27 -0400

    Completed Flask lab 06.

--- code/zach/html-labs/lab06-flask/lab06-flask.py
+++ code/zach/html-labs/lab06-flask/lab06-flask.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ''
+    return render_template('index.html')
+
+@app.route('/password')
+def generate_password():
+    valid_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '-', '_']
+    output_password = ''
+    index = 0
+    password_length = request.args['password_length']
+
+    while index < int(password_length):
+        output_password += random.choice(valid_characters)
+        index += 1
+        
+    return render_template('password.html', password=output_password)
 
 if __name__ == '__main__':
     app.run(debug=True)
