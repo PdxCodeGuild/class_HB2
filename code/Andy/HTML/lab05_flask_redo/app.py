7c3d7fe9894014ef811f357dabd5d7dbf74b4ff5
andy2124 <andygarcia22212@gmail.com>
2022-08-26 17:41:03 -0700

    done

--- code/Andy/HTML/lab05_flask_redo/app.py
+++ code/Andy/HTML/lab05_flask_redo/app.py
@@ -1,22 +1,29 @@
 from flask import Flask, render_template, request
 from requests import post
 
-# app = Flask(__name__, template_folder='templates')
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+# app = Flask(__name__)
 
 # print('type of object: ', type(app))
 # type of object:  <class 'flask.app.Flask'>
 
 @app.route('/')
 def index():
-    return "hey we're here"
-    # return render_template('index.html')
+    # return "hey we're here"
+    return render_template('index.html')
 
 @app.route('/recieve_form/', methods=['GET','POST'])
 def converter():
+    # print('request.POST', request.form.keys())
+    # request.POST dict_keys(['distance', 'in_units'])
+    
     if request.method == 'POST':
-        distance = request.form['unit_distance']
-        in_units = request.form['input_units']
+        distance = request.form['distance']
+        in_units = request.form['in_units']
+        # print('distance type', type(distance))
+        # print('in_units type', type(in_units))
+
+
         
         measurments = {
             'ft': 0.3048,
@@ -29,7 +36,7 @@ def converter():
         }
 
         in_results = measurments[in_units]
-        results = in_results * distance
+        results = in_results * float(distance)
         return(f"{distance} {in_units} is {results} Meters")
 
 app.run(debug=True)
