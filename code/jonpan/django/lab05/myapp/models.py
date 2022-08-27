a8acf8ec5cbbc274efb588230cc7ff2e6cec41dd
Jonathan Pan <jonathan.pan@gmail.com>
2022-08-26 20:14:58 -0700

    added model, still works, but blog posts not up yet via html pages

--- code/jonpan/django/lab05/myapp/models.py
+++ code/jonpan/django/lab05/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    public = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
