263b01e5b7f1bd76ca33bb313f9b16cb491f42f5
andy2124 <andygarcia22212@gmail.com>
2022-08-26 21:31:07 -0700

    register view working going to delete and do it again

--- code/Andy/django_lab/lab_05_blog/blog_app/urls.py
+++ code/Andy/django_lab/lab_05_blog/blog_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+# from .views import register
 
 app_name = 'blog_app'
 urlpatterns = [
-    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
+    path('register/', views.register, name='register'),
 ]
--- code/Andy/django_lab/lab_05_blog/blog_app/views.py
+++ code/Andy/django_lab/lab_05_blog/blog_app/views.py
@@ -1,15 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BlogPost
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 def register(request):
-    print(request.POST)
+    errors = []
 
-    username = request.POST['username']
-
-    return HttpResponse(f'Welcome {username}?')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password_confirm']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                email=request.POST['email'],
+                password=request.POST['password'],
+            )
+            
+            login(request, user)
+            return redirect('/')
+        else:
+            errors.append("Mismatching passwords!")
+    
+    return render(request, 'blog_app/register.html', {'errors': errors})
 
 def signup(request):
     """
