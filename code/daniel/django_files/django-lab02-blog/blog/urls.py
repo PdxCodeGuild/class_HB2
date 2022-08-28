9552c6796353f96316f9083efa52e731ac319028
sushi <dandouglass14@gmail.com>
2022-08-28 19:06:39 -0400

    fix deleted files

--- code/daniel/django_files/django-lab02-blog/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog import views
-
-app_name = 'blog'
-urlpatterns = [
-    path('myview', views.myview, name='myview'),
-]
-
--- code/daniel/django_files/django-lab02-blog/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-
-def myview(request):
-    return HttpResponse('hello world!')
