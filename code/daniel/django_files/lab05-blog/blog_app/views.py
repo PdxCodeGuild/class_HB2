7e416a27692c8ace9689e2d36a608ef2e162d972
daniel <dandouglass14@gmail.com>
2022-09-01 22:43:03 -0400

    lab05 checkpoint

--- code/daniel/django_files/lab05-blog/blog_app/views.py
+++ code/daniel/django_files/lab05-blog/blog_app/views.py
@@ -21,10 +21,28 @@ def index_view(request):
 
 
 def register_view(request):
+    request_method = request.method
+    print("request_method", request_method)
+    if request_method == "POST":
+        print("POST REQUEST")
+        print("request.POST.keys",request.POST.keys)
+        # <built-in method keys of QueryDict object at 0x7f2d674c23b0>
+        print("request.POST.keys",request.POST.keys())
+        # dict_keys(['csrfmiddlewaretoken', 'username', 'password', 'discord'])
+        username = request.POST['username']
+        password = request.POST['password']
+        discord = request.POST['discord']
+        return render(request, 'blog_app/register.html')
+    else:
+        return render(request, 'blog_app/register.html')
+    
 
+    
     # user = User.objects.create_user(username, email=None, password=None)
-    return render(request, 'blog_app/register.html')
+    
 
+# http://127.0.0.1:8000/blog-app/register-page/register
+# http://127.0.0.1:8000/blog-app/register-page/blog-app/register
 
 def login_view(request):
     return render(request, 'blog_app/login.html')
