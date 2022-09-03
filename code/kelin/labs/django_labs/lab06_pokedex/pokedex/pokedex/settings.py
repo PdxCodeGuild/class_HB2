2c743ae86a849af494cb15d8484add33ba08bad4
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-02 18:34:15 -0700

    Django rest framework

--- code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokedex/settings.py
+++ code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokedex/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
 ]
 
 ROOT_URLCONF = 'pokedex.urls'
