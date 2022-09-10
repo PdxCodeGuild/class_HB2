c728e5b20b05c1ac17e27c9b1091f8ef936540ce
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-09 20:12:08 -0700

    Working on Pokedex optional lab

--- code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokemon/models.py
+++ code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokemon/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class PokemonType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Pokemon(request):
+    number = models.IntegerField() 
+    name = models.CharField(max_length=100)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    image_front = models.CharField(max_length=100)
+    image_back = models.CharField(max_length=100)
+    types = models.ManyToManyField()
+
---
+++ code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokemon/urls.py
@@ -0,0 +1,4 @@
+from code.kelin.labs.django_labs.lab06_pokedex.pokedex.pokemon.models import Pokemon
+
+
+app_name = [Pokemon]
