671e7ba8739a59c3ac6c8d1ab496785274966936
Muki-has-ideas <willshelnutt@gmail.com>
2022-09-09 21:06:38 -0700

    commit and push

--- code/muki/Django/lab05/blog_app/models.py
+++ code/muki/Django/lab05/blog_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
-# Create your models here.
-class Blog:
-    stuff = 
+# # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField() 
+    
+    def __str__(self):
+        return self.title
+    
+   
--- code/muki/Django/lab05/blog_project/urls.py
+++ code/muki/Django/lab05/blog_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('blog_app/', include('blog_app.urls')),
+    
     path('admin/', admin.site.urls),
 ]
