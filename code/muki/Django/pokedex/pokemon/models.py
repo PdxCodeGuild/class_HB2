8f28e86dc5b73994d3fd800a0719c8ca0f3063c1
Muki-has-ideas <willshelnutt@gmail.com>
2022-09-09 20:10:45 -0700

    missed one

--- code/muki/Django/pokedex/pokemon/models.py
+++ code/muki/Django/pokedex/pokemon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class PokemonType(models.Model):
+    name = models.CharField(max_length=100)
+
+class Pokemon(models.Model):
+    number =
+    name = 
