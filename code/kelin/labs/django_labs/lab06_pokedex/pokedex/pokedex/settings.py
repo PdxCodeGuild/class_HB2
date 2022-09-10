d7bb57938bca90615322e3f7c008e26acae2c5d6
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-10 11:38:28 -0700

    Working on settings for Pokedex

--- code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokedex/settings.py
+++ code/kelin/labs/django_labs/lab06_pokedex/pokedex/pokedex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     ]
 # }
 
-MEDIA_ROOT = [os.path.join{BASE_DIR, 'media'}]
-MEDIA_URL - '/media/'
+MEDIA_ROOT = [os.path.join{BASE_DIR,'media'}]
+MEDIA_URL = '/media/'
