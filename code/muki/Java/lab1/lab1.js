615f0a2fe9032142dfb14b965186a5c9a96d14cb
Muki-has-ideas <willshelnutt@gmail.com>
2022-09-13 21:24:01 -0700

    commit and checkout

---
+++ code/muki/Java/lab1/lab1.js
@@ -0,0 +1,19 @@
+// Average Numbers using Java 
+
+let digits = [0]
+
+let addend = prompt("Please enter an integer or 'q' to quit:\t")
+
+// if (addend = "q") {
+//     break
+// }
+
+while (addend != q) {
+    let addend = parseInt(addend)
+    digits.push(addend)
+    alert(digits)
+}
+
+if (addend = "q") {
+    break
+}
