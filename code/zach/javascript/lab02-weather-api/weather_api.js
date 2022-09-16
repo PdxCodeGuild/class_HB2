7e91f437c6025e9c5ebc57692e1f8ec108df72b4
Zachary Christman <zachary.christman@8brconsulting.dev>
2022-09-16 00:40:25 -0400

    Added template files and structure for api lab with comments for what I will code.

---
+++ code/zach/javascript/lab02-weather-api/weather_api.js
@@ -0,0 +1,17 @@
+//import key
+
+
+//get location on page load from geolocation api
+
+
+//pass location and key to get request from api
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=imperial
+
+
+//Convert Unix timestamps for date and time to display on page
+
+
+//Display weather
+
+
+//Use icons
