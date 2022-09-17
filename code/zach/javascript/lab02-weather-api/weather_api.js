d08c615355300ef837574e4da9acd887742468e1
Zachary Christman <zachary.christman@8brconsulting.dev>
2022-09-16 23:43:30 -0400

    Completed lab 2 for the weather API.

--- code/zach/javascript/lab02-weather-api/weather_api.js
+++ code/zach/javascript/lab02-weather-api/weather_api.js
@@ -1,17 +1,29 @@
-//import key
+// Pass GetCurrentPosition a function that defines latitude and longitude
+navigator.geolocation.getCurrentPosition(position => {
+    let lat = position.coords.latitude
+    let lon = position.coords.longitude
 
+    // Use axios to execute the API call then pass the response into the html page
+    axios({
+        method: 'get',
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key + '&units=imperial'
+    }).then((response) => {
+        console.log(response.data)
 
-//get location on page load from geolocation api
+        // Get location to display on html page
+        document.getElementById('location').innerHTML = response.data.name
 
+        // Convert unix timestamp to long date/time and display on html page
+        let unix_timestamp = response.data.dt
+        let datetime = new Date(unix_timestamp * 1000)
+        document.getElementById('datetime').innerHTML = datetime
 
-//pass location and key to get request from api
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=imperial
+        // Display weather on html page
+        document.getElementById('weather_description').innerHTML = "It is currently " + response.data.weather['0']['main'] + " and " + response.data.main['temp'] + " degrees Fahrenheit"
 
-
-//Convert Unix timestamps for date and time to display on page
-
-
-//Display weather
-
-
-//Use icons
+        // Create an img element and append to the DOM
+        let img = document.createElement('img')
+        img.src = 'http://openweathermap.org/img/wn/' + response.data.weather['0']['icon'] + '.png'
+        document.getElementById('weather').appendChild(img)
+    })
+})
