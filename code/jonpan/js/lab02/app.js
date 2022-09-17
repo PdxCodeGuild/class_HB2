2123e2fc6c0258cacb8e432bddfd37c1752b0587
notvert <jonathan.pan@gmail.com>
2022-09-16 22:49:10 -0400

    saving work

--- code/jonpan/js/lab02/app.js
+++ code/jonpan/js/lab02/app.js
@@ -1,5 +1,30 @@
-navigator.geolocation.getCurrentPosition(position => {
+function showPosition(position) {
+    document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+  }
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
     console.log(position.coords.latitude)
     console.log(position.coords.longitude)
+    showPosition(position)
 })
+}
 
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(position => {
+    lat = position.coords.latitude
+    long = position.coords.longitude
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}`)
+    
+    // &exclude=hourly,daily
+    // &units=imperial
+
+    .then(function (response) {
+        console.log(response.data);
+
+    // .then(response => response.json())
+    // console.log(response.json)
+    });
+})
+}
