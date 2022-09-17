6b1c61f25746bbe671199234c7566e16ea6ffb09
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-16 19:06:37 -0700

    Add Lat and Long to get weather

---
+++ code/kelin/labs/javascript/weather_api/app_connect.js
@@ -0,0 +1,19 @@
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.latitude)
+    console.log(position.coords.longitude)
+})
