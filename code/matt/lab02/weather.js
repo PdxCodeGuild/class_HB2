0cf6d4e810acdd4a71b42a17ba05e815f884e6c1
Matt Nichols <mudbotmatt@gmail.com>
2022-09-16 21:31:00 -0700

    working

--- code/matt/lab02/weather.js
+++ code/matt/lab02/weather.js
@@ -1,23 +1,32 @@
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition);
-//     } else {
-//         x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-// }
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+        console.log(apiKey)
+        axios({
+            method: 'get',
+            url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}appid=${apiKey}`
+        }).then((response) => {
+            console.log(response.data)
+        })
+    })
+}
+console.log(getLocation())
 
 
-// function showPosition(position) {
-//     x.innerHTML = "Latitude: " + position.coords.latitude +
+// function showPosition() {
+//     document.getElementById("location"), innerHTML = "Latitude: " + position.coords.latitude +
 //         "<br>Longitude: " + position.coords.longitude;
-// }
 
-console.log(apiKey)
-axios({
-    method: 'get',
-    url: `'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}'`
-}).then((response) => {
-    console.log(response);
-}, (error) => {
-    console.log(error);
-});
+
+// console.log(apiKey)
+// axios({
+//     method: 'get',
+//     url: `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`
+// }).then((response) => {
+//     console.log(response.data);
+// }, (error) => {
+//     console.log(error);
+// })
