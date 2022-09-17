1b863876d0e338ae041248a8c57d4ee537828a8e
Matt Nichols <mudbotmatt@gmail.com>
2022-09-16 20:09:59 -0700

    waiting for api key

--- code/matt/lab02/weather.js
+++ code/matt/lab02/weather.js
@@ -1,15 +1,23 @@
-var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-}
 
-axios()
+// function showPosition(position) {
+//     x.innerHTML = "Latitude: " + position.coords.latitude +
+//         "<br>Longitude: " + position.coords.longitude;
+// }
+
+console.log(apiKey)
+axios({
+    method: 'get',
+    url: `'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}'`
+}).then((response) => {
+    console.log(response);
+}, (error) => {
+    console.log(error);
+});
