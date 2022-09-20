be2ada0911bc38d3051d3c38c2b0c3dcce820fd4
Matt Nichols <mudbotmatt@gmail.com>
2022-09-19 21:29:50 -0700

    finished with api lab

--- code/matt/lab02/weather.js
+++ code/matt/lab02/weather.js
@@ -1,32 +1,31 @@
 function getLocation() {
     navigator.geolocation.getCurrentPosition(position => {
-        console.log(position.coords.latitude)
-        console.log(position.coords.longitude)
         let lat = position.coords.latitude
         let lon = position.coords.longitude
-        console.log(apiKey)
+
         axios({
             method: 'get',
-            url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}appid=${apiKey}`
+            url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
         }).then((response) => {
+            let unix_timestamp = (response.data['dt'])
+            let datetime = new Date(unix_timestamp * 1000)
+            let icon = response.data['weather']['0']['icon']
+            let img = document.getElementById('icon')
+            img.src = `http://openweathermap.org/img/wn/${icon}.png`
+            let location = document.getElementById('location')
+            location.innerText = response.data['name']
+            let temp = document.getElementById('temp')
+            temp.innerText = "Current Temp: " + response.data['main']['temp'] + '°F |' + " Feels Like: " + response.data['main']['feels_like'] + '°F'
+            let minMax = document.getElementById('min_max')
+            minMax.innerText = "Min Temp: " + response.data['main']['temp_min'] + '°F |' + " Max Temp: " + response.data['main']['temp_max'] + '°F'
+            let conditions = document.getElementById('conditions')
+            conditions.innerText = response.data['weather']['0']['description'] + ' |' + " Wind: " + response.data['wind']['speed'] + 'MPH'
+            console.log(datetime);
             console.log(response.data)
+
+        }, (error) => {
+            console.log(error);
         })
     })
 }
 console.log(getLocation())
-
-
-// function showPosition() {
-//     document.getElementById("location"), innerHTML = "Latitude: " + position.coords.latitude +
-//         "<br>Longitude: " + position.coords.longitude;
-
-
-// console.log(apiKey)
-// axios({
-//     method: 'get',
-//     url: `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`
-// }).then((response) => {
-//     console.log(response.data);
-// }, (error) => {
-//     console.log(error);
-// })
