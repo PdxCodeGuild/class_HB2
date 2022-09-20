8ab8cab6a4a813aabacffb6d697b50bc27f82ac1
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-19 21:35:33 -0700

    Fixing weather description to load image

--- code/kelin/labs/javascript/weather_api/app_connect.js
+++ code/kelin/labs/javascript/weather_api/app_connect.js
@@ -15,21 +15,23 @@ function showPosition(position) {
 
 function getWeather() {
     navigator.geolocation.getCurrentPosition(position => {
-        console.log(position.coords.latitude)
-        console.log(position.coords.longitude)
+        // console.log(position.coords.latitude)
+        // console.log(position.coords.longitude)
         let lat = position.coords.latitude
         let lon = position.coords.longitude
         axios({
             method: 'get',
             url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
-          })
-          
-          .then((response) => {
+          }).then((response) => {
             console.log(response.data)
-          })
+            document.getElementById('location').innerHTML = response.data.name
+            let unix_timestamp = response.data.dt
+            let datetime = new Date(unix_timestamp * 1000)
+            document.getElementById('datetime').innerHTML = datetime   
+            document.getElementById('weather_description').innerHTML = "It is currently " + response.data.weather['0']['main'] + " and " + response.data.main['temp'] + " degrees Fahrenheit"
+            let img = document.createElement('img')
+            img.src = `http://openweathermap.org/img/wn/${response.data.weather}${['0']['icon']}.png`
+            document.getElementById('weather').appendChild(img)
         })
-
-}
-
-function getWeather()
-  
+    })
+  }
