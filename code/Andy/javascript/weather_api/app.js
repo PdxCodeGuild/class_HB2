526c78f49df6ca4b29a3a5d540f137ff949426f5
andy2124 <andygarcia22212@gmail.com>
2022-09-19 21:36:56 -0700

    almost done

--- code/Andy/javascript/weather_api/app.js
+++ code/Andy/javascript/weather_api/app.js
@@ -1,11 +1,26 @@
-let lat = 3.14
-let lon = 4.13
-
-// let theUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${key}`
-axios({
+navigator.geolocation.getCurrentPosition(position => {
+  let lat = position.coords.latitude
+  let lon = position.coords.longitude
+  
+  
+  
+  // let theUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${key}`
+  axios({
     method: 'get',
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${key}&units=imperial`
   }).then((response) => {
     console.log(response.data)
+    let unix_timestamp = (response.data.dt) // it has to be .dt and not the numbers because then it would just be for my timezone 
+    let datetime = new Date(unix_timestamp*1000)
+    console.log(datetime) // Thu Jun 18 2020 05:21:31 GMT-0700 (Pacific Daylight Time)
+    console.log(response.data.weather[0]['icon'])
+    let icon = response.data.weather[0]['icon']
+    let img = document.getElementById('icon')
+    img.src = `http://openweathermap.org/img/wn/${icon}.png`
+    document.getElementById('temp').innerHTML = response.data.main["temp"] +" F"
+    // temp.innerHTML = response.data.main["temp"]
+    console.log(response.data.name)
+    document.getElementById('city').innerHTML = response.data.name
 
   })  //  add CDN to be able to use this <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+})
