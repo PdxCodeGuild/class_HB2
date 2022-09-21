df3d8aef8371b85a77efd278c8706aba5f39b83f
Zachary Christman <zachary.christman@8brconsulting.dev>
2022-09-21 00:35:20 -0400

    More progress on the calculator app. Need to get the mathmatics to work now and display result.

--- code/zach/javascript/lab03-calculator/calculator.js
+++ code/zach/javascript/lab03-calculator/calculator.js
@@ -1,29 +1,58 @@
+let num = 0
+let digits = []
+let numbers_for_operation = []
+let result = 0
+
 function user_input() {
-    let nums = []
     let buttons = document.querySelectorAll("button")
     buttons.forEach(button => {
         button.addEventListener("click", (event) => {
-            // If button.className == "number" || "decimal" else if == "operator" then call the appropriate function else clear etc.
-            nums.push(event.target.value)
-            console.log(nums)
+            if (button.className == "all-clear") {
+                document.getElementById("calculator-screen").value = 0
+                digits = []
+                numbers_for_operation = []
+                result = 0
+            } else if (button.className == "number" || button.className == "decimal") {
+                digits.push(event.target.value)
+                num = convert_to_num(digits)
+                document.getElementById("calculator-screen").value = num
+            } else if (button.className == "operator") {
+                let operation = button.value
+                numbers_for_operation.push(num)
+                digits = []
+                document.getElementById("calculator-screen").value = ""
+                if (numbers_for_operation.length == 2) {
+                    result = operator(numbers_for_operation[0], numbers_for_operation[1], operation)
+                    console.log(result)
+                }
+            } else {
+                numbers_for_operation.push(num)
+                document.getElementById("calculator-screen").value = result
+            }
         })
     })
 }
 
-function add_nums(num1=0, num2) {
-    return num1 + num2
+function convert_to_num(digits_list) {
+    return parseFloat(digits_list.join(""))
 }
 
-function subtract_nums(num1=0, num2) {
-    return num1 - num2
+function operator(num1, num2, operation) {
+    if (operation == "+") {
+        return num1 + num2
+    } else if (operation == "-") {
+        return num1 - num2
+    } else if (operation == "*") {
+        return num1 - num2
+    } else if (operation == "/") {
+        if (num1 == 0) {
+            return "Invalid"
+        } else {
+            return num1 / num2
+        }
+    } else {
+        
+    }
 }
 
-function divide_nums(num1, num2) {
-    return num1 / num2
-}
-
-function calculator() {
-    user_input()
-}
-
-calculator()
+user_input()
