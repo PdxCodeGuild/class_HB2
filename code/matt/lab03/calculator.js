a2d768c3356e13f68a6828cd49aca67c41c8c1be
Matt Nichols <mudbotmatt@gmail.com>
2022-09-21 21:32:32 -0700

    progress!

--- code/matt/lab03/calculator.js
+++ code/matt/lab03/calculator.js
@@ -1,35 +1,39 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator-keys')
+const display = document.querySelector('.calculator-screen')
+const previousKeyType = calculator.dataset.previousKeyType
 
-keys.addEventListener('click', pro => {
-    if (pro.target.matches('button')) {
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        if (!action) {
+            if (displayedNum === "0") {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            calculator.dataset.previousKeyType = 'operator'
+        }
+
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.'
+        }
+
+        if (action === 'clear') {
+            console.log('clear key!')
+        }
+        if (action === 'calculate') {
+            console.log('Calculator key!')
+        }
     }
 })
-
-const key = pro.target
-const action = key.dataset.action
-
-if (!action) {
-    console.log('number key!')
-}
-
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key!')
-}
-
-if (action === 'decimal') {
-    console.log('decimal key!')
-}
-
-if (action === 'clear') {
-    console.log('clear key!')
-}
-
-if (action === 'calculate') {
-    console.log('equal key!')
-}
