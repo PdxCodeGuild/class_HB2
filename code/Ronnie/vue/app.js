76d4e4968b6fe466eb0be53470acc14ad8330bc6
radmosley <34037881+radmosley@users.noreply.github.com>
2022-09-21 17:53:51 -0700

    add vue example

--- code/Ronnie/vue/app.js
+++ code/Ronnie/vue/app.js
@@ -1,12 +1,3 @@
-const thisAddition = () => {
-    let add = user_input[1] + user_input[2]
-    return value
-}
 
-
-document.addEventListener("click", (event) => {
-    let getAdd = thisAddition(user_input[1], user_input[2])
-    let getSub = thisSubtraction(user_input)
-})
 
 
