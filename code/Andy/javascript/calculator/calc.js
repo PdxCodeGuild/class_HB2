caf94b17fe9ab97bc2c58621be95d1a23855fdc5
andy2124 <andygarcia22212@gmail.com>
2022-09-22 17:52:53 -0700

    saving

--- code/Andy/javascript/calculator/calc.js
+++ code/Andy/javascript/calculator/calc.js
@@ -1,29 +1,48 @@
 const eight = document.getElementById('eight')
+const calcScreen = document.getElementById('calculator-screen')
 const choices = document.querySelectorAll('button')
 console.log(eight)
 console.log(choices)
 let string = ""
-let results = 0
+let results 
 let nums = []
 let operator = document.querySelectorAll('operator')
+let equal = document.querySelectorAll('equal-sign')
+let allClear = document.querySelectorAll('all-clear')
+let calcTest = document.getElementById('calc-test')
 
 let calcBtn 
-choices.forEach(choice=>choice.addEventListener('click', (event)=>{
-    calcBtn=event.target.id
-    if (calcBtn in operator){
-        nums.push(calcBtn)
+choices.forEach(choice=>choice.addEventListener('click', (event)=>{// for each method target all method from
+//    console.log(event,'events')
+    calcBtn=event.target.id // targeting id from input 
+    // console.log(calcBtn, 'user choice')
+    calcScreen.value= calcBtn
+   calcScreen.value = results += calcBtn
+   nums.push(calcBtn)
+   console.log(nums, 'nums')
+   for(i =0; i<nums.length;i++){
+    //    console.log(nums[i],i,'testing nums')
+    if (nums[i]==='+' || "-"||'/'||'*'){
+        console.log('yay')
     }
-    if (calcBtn=='+'){
-        results=results + parseInt(nums.join())
-    } 
-    console.log(results )
-    console.log(nums)
-    // console.log(calcBtn)
-    // string = string + calcBtn;
-    // console.log(string)
-    // document.querySelector('input').value= string
 
+   }
+    
 }))
+// if (calcBtn in operator){
+    //     nums.push(calcBtn)
+    // }
+    // if (calcBtn=='+'){
+        //   results=results + parseInt(nums.join())
+        // } 
+        // console.log(results )
+        // console.log(nums)
+        // console.log(calcBtn)
+        // console.log(string)
+        // string = string + calcBtn;
+// document.querySelector('input').value= string
+
+
 // function(choice){
 //     choice.addEventListener()
 //     choice *2
