56c32a590b8ab072f7ed372daf53b064130d8a44
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-23 21:22:23 -0700

    Added notes about Unit converter

---
+++ code/kelin/labs/javascript/vue_redo/redo.vue
@@ -0,0 +1,50 @@
+<!-- Kelin Ray
+Vue Redo Lab
+Unit Converter -->
+
+<!-- Unit Converter
+Simple version: the user enters the distance and units and the app shows them the converted distance in meters
+Complex version: the user also enters output units -->
+
+<!-- Python code
+from operator import truediv
+
+
+feet_to_meters = {'ft': 0.3048}
+
+number_feet = input("\nPlease enter the number of feet: ")
+
+number_feet = int(number_feet)
+
+meters = (feet_to_meters['ft'] * number_feet)
+
+print((number_feet), "feet is", (meters), "meters")
+
+meter = {
+    "ft": 0.3048,
+    "mi": 1609.34,
+    "m": 1,
+    "km": 1000,
+    "yd": 0.9144,
+    "in": 0.0254
+}
+
+while True:
+    number = input("Enter the distance: ")
+    unit = input("Enter the unit - ft - mi - m - km - yd - in: ")
+    if unit == 'ft':
+        print((number), "feet is", (int(number) * (meter["ft"])), "meters")
+    elif unit == 'mi':
+        print((number), "miles is", (int(number) * (meter["mi"])), "meters")
+    elif unit == 'm':
+        print((number), "meters is", (int(number) * (meter["m"])), "meters")
+    elif unit == 'km':
+        print((number), "kilometers is", (int(number) * (meter["km"])), "meters")
+    elif unit == 'yd':
+        print((number), "yards", (int(number) * (meter["yd"])), "meters")
+    elif unit == 'in':
+        print((number), "inches is", (int(number) * (meter["in"])), "meters")
+    else:
+        print("Invalid unit")
+-->
+
