26837ab3d754557e51067ff39a760f46ac7237f8
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-26 19:40:32 -0700

    Version 1 of Unit converter HTML

--- code/kelin/labs/javascript/vue_redo/redo.vue
+++ code/kelin/labs/javascript/vue_redo/redo.vue
@@ -47,4 +47,40 @@ while True:
     else:
         print("Invalid unit")
 -->
+<script>
+  const app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+                startUnit: '',
+                endUnit: '',
+                distance: '',
+                convertedDistance: '',
+                units: [{
+                    name: 'inches', value: .0254,
+                }, {
+                    name: 'feet', value: .3048,
+                }, {
+                    name: 'yards', value: .9144,
+                }, {
+                    name: 'meters', value: 1,
+                }, {
+                    name: 'miles', value: 1609.34,
+                }, {
+                    name: 'kilometers', value: 1000,
+                }],
+            },
+            methods: {
+                convert: function () {
+                    var unitToMeters = this.distance * this.startUnit.value
+                    this.convertedDistance = unitToMeters / this.endUnit.value 
+                    this.convertedDistance = Math.round((this.convertedDistance + Number.EPSILON) * 100) / 100
+                    this.message = this.distance + ' ' + this.startUnit.name + ' converts to ' + this.convertedDistance + ' ' + this.endUnit.name
+                }
+            },
+            created: function () {
+
+    }
+})
+</script>
 
