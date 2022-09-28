c9e0fe9d6f0476516d1f2e9f725c6bc4c24f4ca3
daniel <dandouglass14@gmail.com>
2022-09-28 00:33:44 -0400

    working on - operator

--- code/daniel/js_files/daniel-calc/calc.js
+++ code/daniel/js_files/daniel-calc/calc.js
@@ -1,12 +1,13 @@
-// add default values
+
 let calcScreen = document.querySelector(".calculator-screen");
 let buttons = document.querySelectorAll("button");
 let keys = document.querySelectorAll(".calculator-keys");
 let operatorPressed = ""
 let numberPressed = []
+let operator = []
 let runningTotal = 0
+let currentValue = numberPressed.join("")
 
-// console.log(buttons[0])
 
 buttons.forEach((button) => {
     button.addEventListener('click',(event) => {
@@ -19,47 +20,52 @@ buttons.forEach((button) => {
                 console.log(runningTotal)
                 runningTotal = 0
             }
-            runningTotal = eval(runningTotal + buttonValue + numberPressed.join(""))
-            numberPressed = []
-            operatorPressed = buttonValue
-            }
-// ===========
-            // let currentValue = numberPressed.join("")
-//             if (event.target.value == "+") {
-//                 runningTotal = parseInt(runningTotal) + parseInt(currentValue)
-//                 numberPressed = []
-//                 operatorPressed = event.target.value
-//                 console.log(runningTotal)
-//             } else if (event.target.value == "-") {
-//                 runningTotal = parseInt(runningTotal) - parseInt(currentValue)
-//                 numberPressed = []
-//                 console.log(runningTotal)
-//             } else if (event.target.value == "*") {
-//                 runningTotal = parseInt(runningTotal) * parseInt(currentValue)
-//                 numberPressed = []
-//                 console.log(runningTotal) 
-//             } else if (event.target.value == "/") {
-//                 runningTotal = parseInt(runningTotal) / parseInt(currentValue)
-//                 numberPressed = []
-//                 console.log(runningTotal)
-//             } else if (event.target.value == "=") {
-//                 runningTotal = eval(runningTotal + operatorPressed + numberPressed.join("")) 
-//                 console.log(runningTotal)
-//             }
+            
+            if (event.target.value == "+") {
+                console.log(runningTotal)
+                runningTotal = eval(runningTotal + buttonValue + numberPressed.join(""))
+                numberPressed = []
+                operatorPressed = buttonValue
+                console.log(runningTotal)
 
-// ============
-            // console.log(buttonValue)
+
+            } else if (event.target.value == "-") {
+                if (runningTotal == 0) {
+                    console.log(runningTotal)
+                    numberPressed.push(buttonValue)
+                    runningTotal = eval(numberPressed + buttonValue + numberPressed.join(""))
+                } else {
+                    !!!!!!!!!!!!!!!!!!!!!! needs work !!!!!!!!!!!!!!!!!!!!!!!!
+                }
+
+                    numberPressed.push(buttonValue)
+                    operator.push(event.target.value)
+                runningTotal = eval(runningTotal + operator + numberPressed.join(""))
+                numberPressed = []
+                operator = []
+                
+                console.log(runningTotal)
+            } else if (event.target.value == "*") {
+                runningTotal = eval(runningTotal + buttonValue + numberPressed.join(""))
+                numberPressed = []
+                operatorPressed = buttonValue
+                console.log(runningTotal) 
+            } else if (event.target.value == "/") {
+                runningTotal = eval(runningTotal + buttonValue + numberPressed.join(""))
+                numberPressed = []
+                operatorPressed = buttonValue
+                console.log(runningTotal)
+            } 
+            
+        }
+
+            
         if (!isNaN(checkIfNum) || buttonValue == '.') {
             console.log(numberPressed)
             numberPressed.push(buttonValue)
-            // console.log(buttonValue)
+            
         }
-        
-
-        // console.log(operatorPressed)
-        // console.log(numberPressed)
     })
-
 })
 
 
