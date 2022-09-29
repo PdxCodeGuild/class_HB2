b773cb617c9e8d556a057c854be2aa401fab86d4
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-09-28 18:48:10 -0700

    Adding user views to API

--- code/kelin/labs/django_labs/lab06_pokedex/pokedex/api/serializers.py
+++ code/kelin/labs/django_labs/lab06_pokedex/pokedex/api/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from pokemon.models import Pokemon, PokemonType
+from django.contrib.auth import get_user_model
 
 class NestedTypeSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('name',)
+        fields = ('type',)
         model = PokemonType
 
+class NestedPokemonSerializer(serializers.ModelSerializer)
+    class Meta:
+        fields = ('name')
+        model = Pokemon
 class PokemonSerializer(serializers.ModelSerializer):
     type_info = NestedTypeSerializer(many=True, source='types')
     class Meta:
@@ -14,5 +19,10 @@ class PokemonSerializer(serializers.ModelSerializer):
 
 class TypeSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('name',)
+        fields = ('id', 'type', 'pokemon')
         model = PokemonType
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'caught', 'username')
+        model = get_user_model()
--- code/kelin/labs/django_labs/lab06_pokedex/pokedex/api/urls.py
+++ code/kelin/labs/django_labs/lab06_pokedex/pokedex/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-
-from .views import PokemonViewSet, TypeViewSet
+from .views import PokemonViewSet, TypeViewSet, UserView, CurrentUser
 
 router = DefaultRouter()
 router.register('pokemon', PokemonViewSet, basename='pokemon')
-router.register('types', TypeViewSet, basename='types')
+router.register('type', TypeViewSet, basename='type')
+router.register('users', UserView, basename='users')
 
 urlpatterns = router.urls + [
-
+    path('currentuser/', CurrentUser.as_view())
 ]
