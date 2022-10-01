0ffe0aa86f46cb5180385a073ee311e737731728
justin-young5 <104773737+justin-young5@users.noreply.github.com>
2022-09-30 20:13:08 -0500

    worked on vue

---
+++ code/justin/Django/PokeVue/pokemon/views.py
@@ -0,0 +1,16 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from pokemon.models import Pokemon, PokemonType
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def pokeview(request):
+    return render(request, "pokemon/poketemp.html")
+
+def pokemon(request):
+    poke = {}
+    poke_gen = Pokemon.objects.all()
+    for p in poke_gen:
+        
+
+    return JsonResponse(request, 'pokemon/index.html',responsedata)
