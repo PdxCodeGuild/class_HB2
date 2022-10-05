0ba9cf423017f65bd24ea2e21480105f83dbef2d
radmosley <34037881+radmosley@users.noreply.github.com>
2022-10-04 20:01:33 -0700

    docker-django-vue example

---
+++ code/Ronnie/docker_django_vue/frontend/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ContactView from '../views/ContactView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ code/Ronnie/docker_django_vue/frontend/src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="contact">
+    <h1>Contact</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ContactView'
+  }
+  </script>
+  
