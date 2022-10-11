fb6b36ad4754a911344649bf962c22e11916447a
violette-git <IDEASFORYANG@GMAIL.COM>
2022-10-10 19:58:57 -0500

    Revert "Jonpan js lab05"

--- code/jonpan/js/lab05/pokeapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'pokeapp'
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("pokemon/", views.pokemon, name="pokemon"),
-]
