254ad9dfad497418fb896818b2081592cc0e6ab0
Zachary Christman <zachary.christman@8brconsulting.dev>
2022-10-14 00:13:29 -0400

    On final step of the pokedex.

--- code/zach/javascript/lab05-pokedex/pokeapp/views.py
+++ code/zach/javascript/lab05-pokedex/pokeapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Pokemon, PokemonType
 from .serializers import PokemonSerializer, TypeSerializer
+from django.shortcuts import render
 
 # Create your views here.
 class PokemonViewSet(viewsets.ModelViewSet):
@@ -10,9 +11,15 @@ class TypeViewSet(viewsets.ModelViewSet):
     queryset = PokemonType.objects.all()
     serializer_class = TypeSerializer
 
-#def pokemon(request):
-#    pokemon = Pokemon.objects.all()
-#    context = {
-#        'pokemon': pokemon
-#    }
-#    return render(request, 'pokeapp/pokemon.html', context)
+def pokemon(request):
+    pokemon = Pokemon.objects.all()
+    context = {
+        'pokemon': pokemon
+    }
+    return render(request, 'index.html', context)
+
+def PokemonSearch(request):
+    if 'thingy' in request.GET and request.GET['thingy']:
+        pokemon_list = request.GET['thingy']
+    else: 
+        pokemon_list = Pokemon.objects.filter()
--- code/zach/javascript/lab05-pokedex/pokeproj/urls.py
+++ code/zach/javascript/lab05-pokedex/pokeproj/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from pokeapp.views import pokemon
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pokeapp/', include('pokeapp.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='index')
+    path('', pokemon, name='index')
 ]
